5f09c7844e5396d7c320a20a8a1d2e40b74482df
Bart Feenstra <bart@mynameisbart.com>
2019-02-28 21:22:58 +0000

    Import event types and dates.

--- betty/ancestry.py
+++ betty/ancestry.py
@@ -1,4 +1,6 @@
-from typing import Dict
+import calendar
+from enum import Enum
+from typing import Dict, Optional
 
 
 class Entity:
@@ -39,8 +41,74 @@ class Place(Entity):
         return self._name or 'unknown'
 
 
+class Date:
+    def __init__(self, year: int, month: int = None, day: int = None):
+        self._year = year
+        self._month = month
+        self._day = day
+
+    @property
+    def year(self) -> int:
+        return self._year
+
+    @property
+    def month(self) -> Optional[int]:
+        return self._month
+
+    @property
+    def day(self) -> Optional[int]:
+        return self._day
+
+    @property
+    def label(self) -> str:
+        if self._year and self._month and self._day:
+            return '%s %d, %d' % (calendar.month_name[self._month], self._day, self._year)
+        if not self._year and self._month and self._day:
+            return '%s %d' % (calendar.month_name[self._month], self._day)
+        if self._year and self._month and not self._day:
+            return '%s, %d' % (calendar.month_name[self._month], self._year)
+        return 'unknown'
+
+
 class Event(Entity):
-    pass
+    class Type(Enum):
+        BIRTH = 'birth'
+        DEATH = 'death'
+        MARRIAGE = 'marriage'
+
+    _type_labels = {
+        Type.BIRTH: 'Birth',
+        Type.DEATH: 'Death',
+        Type.MARRIAGE: 'Marriage',
+    }
+
+    def __init__(self, entity_id: str, entity_type: Type):
+        Entity.__init__(self, entity_id)
+        self._date = None
+        self._type = entity_type
+
+    @property
+    def label(self) -> str:
+        type_label = self._type_labels[self._type]
+        if self._date:
+            return '%s (%s)' % (type_label, self._date.label)
+        return type_label
+
+    @property
+    def date(self) -> Optional[Date]:
+        return self._date
+
+    @date.setter
+    def date(self, date: Date):
+        self._date = date
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, event_type: Type):
+        self._type = event_type
 
 
 class Ancestry:
