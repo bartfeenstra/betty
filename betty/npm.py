d686bc94b1af915eb325e1673bd0979e7a1378e2
Bart Feenstra <bart@mynameisbart.com>
2019-03-12 22:26:42 +0000

    Simplify 'npm install' invocations.

--- betty/npm.py
+++ betty/npm.py
@@ -2,23 +2,21 @@ import hashlib
 from os import makedirs
 from os.path import join, expanduser
 from shutil import copy2
-from subprocess import Popen, PIPE
+from subprocess import Popen
 
 import betty
 from betty import RESOURCE_PATH
 
 _BETTY_INSTANCE_ID = hashlib.sha1(betty.__path__[0].encode()).hexdigest()
-_BETTY_INSTANCE_NPM_DIR = join(expanduser('~'), '.betty', _BETTY_INSTANCE_ID)
+BETTY_INSTANCE_NPM_DIR = join(expanduser('~'), '.betty', _BETTY_INSTANCE_ID)
 
 
-def install() -> None:
+def install(only: str = 'prod') -> None:
     ensure_target()
-    p = Popen(['npm', 'install', '--only', 'prod'], cwd=_BETTY_INSTANCE_NPM_DIR,
-              stdout=PIPE, stderr=PIPE)
-    p.wait()
+    Popen(['npm', 'install', '--only=%s' % only], cwd=BETTY_INSTANCE_NPM_DIR).wait()
 
 
 def ensure_target() -> None:
-    makedirs(_BETTY_INSTANCE_NPM_DIR, 0o700, True)
+    makedirs(BETTY_INSTANCE_NPM_DIR, 0o700, True)
     copy2(join(RESOURCE_PATH, 'package.json'), join(
-        _BETTY_INSTANCE_NPM_DIR, 'package.json'))
+        BETTY_INSTANCE_NPM_DIR, 'package.json'))
