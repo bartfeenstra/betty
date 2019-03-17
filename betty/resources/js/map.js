95aaeac593392a45aa9db3a73d47b0abdffc6057
Bart Feenstra <bart@mynameisbart.com>
2019-03-17 14:44:24 +0000

    Fix the map attribution.

--- betty/resources/js/map.js
+++ betty/resources/js/map.js
@@ -10,10 +10,7 @@ function initializeMaps () {
     console.log(mapContainer.dataset)
     let map = L.map(mapContainer.id).setView([mapContainer.dataset.latitude, mapContainer.dataset.longitude], 13)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-      // maxZoom: 18,
-      // id: 'mapbox.streets',
-      // accessToken: 'your.mapbox.access.token'
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
   }
 }
