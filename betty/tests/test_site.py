21d899ffcc54b658e8c2901a9b33bc8073a65f87
Bart Feenstra <bart@mynameisbart.com>
2019-03-19 09:51:18 +0000

    Test the site module.

---
+++ betty/tests/test_site.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from betty.ancestry import Ancestry
+from betty.config import Configuration
+from betty.site import Site
+
+
+class SiteTEst(TestCase):
+    def test_ancestry_should_return(self):
+        ancestry = Ancestry()
+        configuration = Mock(Configuration)
+        sut = Site(ancestry, configuration)
+        self.assertEquals(sut.ancestry, ancestry)
+
+    def test_configuration_should_return(self):
+        ancestry = Ancestry()
+        configuration = Mock(Configuration)
+        sut = Site(ancestry, configuration)
+        self.assertEquals(sut.configuration, configuration)
