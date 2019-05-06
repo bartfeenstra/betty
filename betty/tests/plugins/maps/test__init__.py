c6ac9cb7d446d32bc34b0522574187b64638c614
Bart Feenstra <bart@mynameisbart.com>
2019-05-06 15:36:51 +0100

    Reduce test complexity.

--- betty/tests/plugins/maps/test__init__.py
+++ betty/tests/plugins/maps/test__init__.py
@@ -8,29 +8,17 @@ from betty.render import render
 from betty.site import Site
 
 
-class RenderTest(TestCase):
-    _outputDirectory = None
-    site = None
-
-    @classmethod
-    def setUpClass(cls):
-        cls._outputDirectory = TemporaryDirectory()
-        configuration = Configuration(cls._outputDirectory.name, 'https://ancestry.example.com')
-        configuration.plugins[Maps] = {}
-        cls.site = Site(configuration)
-        render(cls.site)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls._outputDirectory.cleanup()
-
-    def test_betty_js(self):
-        with open(join(self.__class__._outputDirectory.name, 'betty.js')) as f:
-            betty_js = f.read()
-        self.assertIn('maps.js', betty_js)
-        self.assertIn('maps.css', betty_js)
-
-    def test_betty_css(self):
-        with open(join(self.__class__._outputDirectory.name, 'betty.css')) as f:
-            betty_css = f.read()
-        self.assertIn('.map', betty_css)
+class MapsTest(TestCase):
+    def test_post_render_event(self):
+        with TemporaryDirectory() as output_directory_path:
+            configuration = Configuration(output_directory_path, 'https://ancestry.example.com')
+            configuration.plugins[Maps] = {}
+            site = Site(configuration)
+            render(site)
+            with open(join(output_directory_path, 'betty.js')) as f:
+                betty_js = f.read()
+            self.assertIn('maps.js', betty_js)
+            self.assertIn('maps.css', betty_js)
+            with open(join(output_directory_path, 'betty.css')) as f:
+                betty_css = f.read()
+            self.assertIn('.map', betty_css)
