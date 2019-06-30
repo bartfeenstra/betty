5f91c9bd15aeba30ad86063cd722b711cfa88ff7
Bart Feenstra <bart@mynameisbart.com>
2019-06-30 12:08:39 +0100

    Make the command line log to stderr.

---
+++ betty/logging.py
@@ -0,0 +1,25 @@
+from logging import CRITICAL, ERROR, WARNING, INFO, DEBUG, NOTSET, StreamHandler
+
+
+class CliHandler(StreamHandler):
+    COLOR_LEVELS = [
+        (CRITICAL, 91),
+        (ERROR, 91),
+        (WARNING, 93),
+        (INFO, 92),
+        (DEBUG, 97),
+        (NOTSET, 97),
+    ]
+
+    def __init__(self):
+        StreamHandler.__init__(self)
+
+    def format(self, record):
+        s = StreamHandler.format(self, record)
+        for level, color in self.COLOR_LEVELS:
+            if record.levelno >= level:
+                return self._color(s, color)
+        return s
+
+    def _color(self, s: str, color: int) -> str:
+        return '\033[%dm%s\033[0m' % (color, s)
