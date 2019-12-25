e3857b6454d6b6353b6f5a75b95f84fecc3accf5
Bart Feenstra <bart@mynameisbart.com>
2019-12-25 02:20:53 +0000

    Add nginx configuration for deployments.

--- betty/plugins/nginx/__init__.py
+++ betty/plugins/nginx/__init__.py
@@ -1,32 +1,64 @@
-from os.path import join, dirname
+import os
+from shutil import copyfile
 from typing import List, Tuple, Callable, Type, Dict, Optional
 
+from voluptuous import Schema, Required, Any
+
+from betty.config import validate_configuration
 from betty.jinja2 import render_file, create_environment
 from betty.plugin import Plugin
 from betty.render import PostRenderEvent
 from betty.site import Site
 
+DOCKER_PATH = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'resources',
+                           'docker')
+
+ConfigurationSchema = Schema({
+    Required('content_negotiation', default=False): bool,
+    Required('www_directory_path', default=None): Any(None, str),
+    Required('https', default=None): Any(None, bool),
+})
+
 
 class Nginx(Plugin):
-    def __init__(self, site: Site):
+    def __init__(self, site: Site, www_directory_path: Optional[str] = None, https: Optional[bool] = None):
+        self._https = https
+        self._www_directory_path = www_directory_path
         self._site = site
 
     @classmethod
     def from_configuration_dict(cls, site: Site, configuration: Dict):
-        return cls(site)
+        configuration = validate_configuration(ConfigurationSchema, configuration)
+        return cls(site, configuration['www_directory_path'], configuration['https'])
 
     def subscribes_to(self) -> List[Tuple[Type, Callable]]:
         return [
             (PostRenderEvent, lambda event: self._render_config()),
+            (PostRenderEvent, lambda event: self._copy_dockerfile()),
         ]
 
     @property
     def resource_directory_path(self) -> Optional[str]:
-        return '%s/resources' % dirname(__file__)
+        return '%s/resources' % os.path.dirname(__file__)
+
+    @property
+    def https(self) -> bool:
+        if self._https is None:
+            return self._site.configuration.base_url.startswith('https')
+        return self._https
+
+    @property
+    def www_directory_path(self) -> str:
+        if self._www_directory_path is None:
+            return self._site.configuration.www_directory_path
+        return self._www_directory_path
 
     def _render_config(self) -> None:
         file_name = 'nginx.conf.j2'
-        destination_file_path = join(
-            self._site.configuration.output_directory_path, file_name)
+        destination_file_path = os.path.join(self._site.configuration.output_directory_path, file_name)
         self._site.resources.copy2(file_name, destination_file_path)
         render_file(destination_file_path, create_environment(self._site))
+
+    def _copy_dockerfile(self) -> None:
+        copyfile(os.path.join(DOCKER_PATH, 'Dockerfile'),
+                 os.path.join(self._site.configuration.output_directory_path, 'Dockerfile'))
