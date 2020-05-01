0342fd1f683b63e35d28ee6bee4c8ad89bb4b5c7
Bart Feenstra <bart@mynameisbart.com>
2020-05-02 00:46:18 +0100

    Add Python 3.6 and 3.7 compatibility.

--- betty/tests/__init__.py
+++ betty/tests/__init__.py
@@ -1,3 +1,4 @@
+from contextlib import suppress
 from tempfile import TemporaryDirectory
 
 import betty
@@ -6,11 +7,14 @@ import betty
 def patch_cache(f):
     def _patch_cache(*args, **kwargs):
         original_cache_directory_path = betty._CACHE_DIRECTORY_PATH
-        with TemporaryDirectory() as cache_directory_path:
-            try:
-                betty._CACHE_DIRECTORY_PATH = cache_directory_path
-                f(*args, **kwargs)
-            finally:
-                betty._CACHE_DIRECTORY_PATH = original_cache_directory_path
+        cache_directory = TemporaryDirectory()
+        betty._CACHE_DIRECTORY_PATH = cache_directory.name
+        try:
+            f(*args, **kwargs)
+        finally:
+            betty._CACHE_DIRECTORY_PATH = original_cache_directory_path
+            # Pythons 3.6 and 3.7 do not allow the temporary directory to have been removed already.
+            with suppress(FileNotFoundError):
+                cache_directory.cleanup()
 
     return _patch_cache
