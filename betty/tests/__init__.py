692a0c5fe61ab1a834dc36c3fc20d5c9683e8135
Bart Feenstra <bart@mynameisbart.com>
2020-05-02 00:29:46 +0100

    Simplify the code.

--- betty/tests/__init__.py
+++ betty/tests/__init__.py
@@ -5,13 +5,12 @@ import betty
 
 def patch_cache(f):
     def _patch_cache(*args, **kwargs):
-        try:
-            original_cache_directory_path = betty._CACHE_DIRECTORY_PATH
-            cache_directory = TemporaryDirectory()
-            betty._CACHE_DIRECTORY_PATH = cache_directory.name
-            f(*args, **kwargs)
-        finally:
-            betty._CACHE_DIRECTORY_PATH = original_cache_directory_path
-            cache_directory.cleanup()
+        original_cache_directory_path = betty._CACHE_DIRECTORY_PATH
+        with TemporaryDirectory() as cache_directory_path:
+            try:
+                betty._CACHE_DIRECTORY_PATH = cache_directory_path
+                f(*args, **kwargs)
+            finally:
+                betty._CACHE_DIRECTORY_PATH = original_cache_directory_path
 
     return _patch_cache
