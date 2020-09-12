bfc3bb15259d86444bfe1a46681b5206f91a4630
Bart Feenstra <bart@mynameisbart.com>
2020-09-12 15:35:46 +0100

    Replace the individual template test base classes with a unified template testing API.

--- betty/tests/__init__.py
+++ betty/tests/__init__.py
@@ -1,7 +1,13 @@
-from contextlib import suppress
+from contextlib import suppress, asynccontextmanager
 from tempfile import TemporaryDirectory
+from typing import Optional, Dict, Callable, Tuple
+from unittest import TestCase
+
+from jinja2 import Environment, Template
 
 import betty
+from betty.config import Configuration
+from betty.site import Site
 
 
 def patch_cache(f):
@@ -18,3 +24,43 @@ def patch_cache(f):
                 cache_directory.cleanup()
 
     return _patch_cache
+
+
+class TemplateTestCase(TestCase):
+    template_string = None
+    template_file = None
+
+    def __init__(self, *args, **kwargs):
+        TestCase.__init__(self, *args, **kwargs)
+        if self.template_string is not None and self.template_file is not None:
+            class_name = self.__class__.__name__
+            raise RuntimeError(f'{class_name} must define either `{class_name}.template_string` or `{class_name}.template_file`, but not both.')
+
+    @asynccontextmanager
+    async def _render(self, data: Optional[Dict] = None, template_file: Optional[Template] = None, template_string: Optional[str] = None, update_configuration: Optional[Callable[[Configuration], None]] = None) -> Tuple[str, Site]:
+        if template_string is not None and template_file is not None:
+            raise RuntimeError('You must define either `template_string` or `template_file`, but not both.')
+        if template_string is not None:
+            template = template_string
+            template_factory = Environment.from_string
+        elif template_file is not None:
+            template = template_file
+            template_factory = Environment.get_template
+        elif self.template_string is not None:
+            template = self.template_string
+            template_factory = Environment.from_string
+        elif self.template_file is not None:
+            template = self.template_file
+            template_factory = Environment.get_template
+        else:
+            class_name = self.__class__.__name__
+            raise RuntimeError(f'You must define one of `template_string`, `template_file`, `{class_name}.template_string`, or `{class_name}.template_file`.')
+        if data is None:
+            data = {}
+        with TemporaryDirectory() as output_directory_path:
+            configuration = Configuration(output_directory_path, 'https://example.com')
+            configuration.mode = 'development'
+            if update_configuration is not None:
+                update_configuration(configuration)
+            async with Site(configuration) as site:
+                yield await template_factory(site.jinja2_environment, template).render_async(**data), site
--- betty/tests/assets/templates/meta/test_place_html_j2.py
+++ betty/tests/assets/templates/meta/test_place_html_j2.py
@@ -1,17 +1,19 @@
 from betty.ancestry import PlaceName, Place, Enclosure
 from betty.functools import sync
-from betty.tests.assets.templates import TemplateTestCase
+from betty.tests import TemplateTestCase
 
 
 class Test(TemplateTestCase):
-    template = 'meta/place.html.j2'
+    template_file = 'meta/place.html.j2'
 
     @sync
     async def test_without_enclosing_places(self):
         place = Place('P0', [PlaceName('The Place')])
         expected = '<div class="meta"></div>'
-        actual = await self._render(place=place)
-        self.assertEqual(expected, actual)
+        async with self._render(data={
+            'place': place,
+        }) as (actual, _):
+            self.assertEqual(expected, actual)
 
     @sync
     async def test_with_enclosing_place_without_place_context(self):
@@ -21,8 +23,10 @@ class Test(TemplateTestCase):
         all_enclosing_place = Place('P2', [PlaceName('The All-enclosing Place')])
         Enclosure(enclosing_place, all_enclosing_place)
         expected = '<div class="meta">in <address><a href="/place/P1/index.html"><span>The Enclosing Place</span></a></address>, <address><a href="/place/P2/index.html"><span>The All-enclosing Place</span></a></address></div>'
-        actual = await self._render(place=place)
-        self.assertEqual(expected, actual)
+        async with self._render(data={
+            'place': place,
+        }) as (actual, _):
+            self.assertEqual(expected, actual)
 
     @sync
     async def test_with_enclosing_place_with_matching_place_context(self):
@@ -32,8 +36,11 @@ class Test(TemplateTestCase):
         all_enclosing_place = Place('P2', [PlaceName('The All-enclosing Place')])
         Enclosure(enclosing_place, all_enclosing_place)
         expected = '<div class="meta">in <address><a href="/place/P1/index.html"><span>The Enclosing Place</span></a></address></div>'
-        actual = await self._render(place=place, place_context=all_enclosing_place)
-        self.assertEqual(expected, actual)
+        async with self._render(data={
+            'place': place,
+            'place_context': all_enclosing_place,
+        }) as (actual, _):
+            self.assertEqual(expected, actual)
 
     @sync
     async def test_with_enclosing_place_with_non_matching_place_context(self):
@@ -44,5 +51,8 @@ class Test(TemplateTestCase):
         Enclosure(enclosing_place, all_enclosing_place)
         unrelated_place = Place('P999', [PlaceName('Far Far Away')])
         expected = '<div class="meta">in <address><a href="/place/P1/index.html"><span>The Enclosing Place</span></a></address>, <address><a href="/place/P2/index.html"><span>The All-enclosing Place</span></a></address></div>'
-        actual = await self._render(place=place, place_context=unrelated_place)
-        self.assertEqual(expected, actual)
+        async with self._render(data={
+            'place': place,
+            'place_context': unrelated_place,
+        }) as (actual, _):
+            self.assertEqual(expected, actual)
