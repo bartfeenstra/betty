fd0f2e5fab3140f1805a7cf8a511f48a31a2cc49
Bart Feenstra <bart@mynameisbart.com>
2020-09-25 14:27:43 +0100

    Localize trees.

--- betty/plugin/trees/assets/js/trees.js
+++ betty/plugin/trees/assets/js/trees.js
@@ -4,7 +4,6 @@ import treesStyle from './trees.css' // eslint-disable-line no-unused-vars
 
 import cytoscape from 'cytoscape'
 import dagre from 'cytoscape-dagre'
-import people from './people.json'
 
 cytoscape.use(dagre)
 
@@ -15,74 +14,78 @@ function initializeAncestryTrees () {
   }
 }
 
-function initializeAncestryTree (container, personId) {
-  const elements = {
-    nodes: [],
-    edges: []
-  }
-  const person = people[personId]
-  personToNode(person, elements.nodes)
-  parentsToElements(person, elements)
-  childrenToElements(person, elements)
-  const cy = cytoscape({
-    container: document.getElementsByClassName('tree')[0],
-    layout: {
-      name: 'dagre'
-    },
-    wheelSensitivity: 0.25,
-    style: [
-      {
-        selector: 'node',
-        style: {
-          content: 'data(label)',
-          shape: 'round-rectangle',
-          'text-valign': 'center',
-          'text-halign': 'center',
-          'background-color': '#eee',
-          width: 'label',
-          height: 'label',
-          padding: '9px'
-        }
-      },
-      {
-        selector: 'node.public',
-        style: {
-          color: '#149988'
-        }
-      },
-      {
-        selector: 'node.public.hover',
-        style: {
-          color: '#2a615a'
-        }
-      },
-      {
-        selector: 'edge',
-        style: {
-          'curve-style': 'taxi',
-          'taxi-direction': 'downward',
-          width: 4,
-          'target-arrow-shape': 'triangle',
-          'line-color': '#777',
-          'target-arrow-color': '#777'
-        }
+function initializeAncestryTree (tree, personId) {
+  fetch(tree.dataset.bettyPeople)
+    .then((response) => response.json())
+    .then((people) => {
+      const elements = {
+        nodes: [],
+        edges: []
       }
-    ],
-    elements: elements
-  })
-  cy.zoom({
-    level: 1,
-    position: cy.getElementById(personId).position()
-  })
-  cy.on('mouseover', 'node.public', (event) => {
-    event.target.addClass('hover')
-  })
-  cy.on('mouseout', 'node.public', (event) => {
-    event.target.removeClass('hover')
-  })
-  cy.on('tap', 'node.public', (event) => {
-    window.location = event.target.data().url
-  })
+      const person = people[personId]
+      personToNode(person, elements.nodes)
+      parentsToElements(person, elements, people)
+      childrenToElements(person, elements, people)
+      const cy = cytoscape({
+        container: document.getElementsByClassName('tree')[0],
+        layout: {
+          name: 'dagre'
+        },
+        wheelSensitivity: 0.25,
+        style: [
+          {
+            selector: 'node',
+            style: {
+              content: 'data(label)',
+              shape: 'round-rectangle',
+              'text-valign': 'center',
+              'text-halign': 'center',
+              'background-color': '#eee',
+              width: 'label',
+              height: 'label',
+              padding: '9px'
+            }
+          },
+          {
+            selector: 'node.public',
+            style: {
+              color: '#149988'
+            }
+          },
+          {
+            selector: 'node.public.hover',
+            style: {
+              color: '#2a615a'
+            }
+          },
+          {
+            selector: 'edge',
+            style: {
+              'curve-style': 'taxi',
+              'taxi-direction': 'downward',
+              width: 4,
+              'target-arrow-shape': 'triangle',
+              'line-color': '#777',
+              'target-arrow-color': '#777'
+            }
+          }
+        ],
+        elements: elements
+      })
+      cy.zoom({
+        level: 1,
+        position: cy.getElementById(personId).position()
+      })
+      cy.on('mouseover', 'node.public', (event) => {
+        event.target.addClass('hover')
+      })
+      cy.on('mouseout', 'node.public', (event) => {
+        event.target.removeClass('hover')
+      })
+      cy.on('tap', 'node.public', (event) => {
+        window.location = event.target.data().url
+      })
+    })
 }
 
 function personToNode (person, nodes) {
@@ -99,7 +102,7 @@ function personToNode (person, nodes) {
   })
 }
 
-function parentsToElements (child, elements) {
+function parentsToElements (child, elements, people) {
   for (const parentId of child.parentIds) {
     const parent = people[parentId]
     elements.edges.push({
@@ -109,11 +112,11 @@ function parentsToElements (child, elements) {
       }
     })
     personToNode(parent, elements.nodes)
-    parentsToElements(parent, elements)
+    parentsToElements(parent, elements, people)
   }
 }
 
-function childrenToElements (parent, elements) {
+function childrenToElements (parent, elements, people) {
   for (const childId of parent.childIds) {
     const child = people[childId]
     elements.edges.push({
@@ -123,7 +126,7 @@ function childrenToElements (parent, elements) {
       }
     })
     personToNode(child, elements.nodes)
-    childrenToElements(child, elements)
+    childrenToElements(child, elements, people)
   }
 }
 
