9f2e526ff4c4ec8dce6d61153d9945de0f2fcf25
Bart Feenstra <bart@mynameisbart.com>
2020-11-22 13:11:03 +0000

    Add README badges. (#593)

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ SETUP = {
     'url': 'https://github.com/bartfeenstra/betty',
     'classifiers': [
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: MacOS',
         'Operating System :: POSIX :: BSD',
         'Operating System :: POSIX :: Linux',
