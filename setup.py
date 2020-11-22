4c25dd8124b1cc5209abbd84efe7385314a50e98
Bart Feenstra <bart@mynameisbart.com>
2020-11-22 13:12:31 +0000

    Test on Python 3.10.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ SETUP = {
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: JavaScript',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
