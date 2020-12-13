92921a162286676936990bfde15a31007a51d780
Bart Feenstra <bart@mynameisbart.com>
2020-12-13 19:06:46 +0000

    Improve error reporting for extensions' configuration, and make parts of the configuation API public.

--- betty/tests/extension/nginx/test__init__.py
+++ betty/tests/extension/nginx/test__init__.py
@@ -35,7 +35,7 @@ class NginxTest(TestCase):
         with TemporaryDirectory() as output_directory_path:
             configuration = Configuration(
                 output_directory_path, 'http://example.com')
-            configuration.extensions[Nginx] = {}
+            configuration.extensions[Nginx] = Nginx.configuration_schema({})
             expected = r'''
 server {
     listen 80;
@@ -70,7 +70,7 @@ server {
         with TemporaryDirectory() as output_directory_path:
             configuration = Configuration(
                 output_directory_path, 'http://example.com')
-            configuration.extensions[Nginx] = {}
+            configuration.extensions[Nginx] = Nginx.configuration_schema({})
             configuration.clean_urls = True
             expected = r'''
 server {
@@ -106,7 +106,7 @@ server {
         with TemporaryDirectory() as output_directory_path:
             configuration = Configuration(
                 output_directory_path, 'http://example.com')
-            configuration.extensions[Nginx] = {}
+            configuration.extensions[Nginx] = Nginx.configuration_schema({})
             configuration.locales.clear()
             configuration.locales['en-US'] = LocaleConfiguration('en-US', 'en')
             configuration.locales['nl-NL'] = LocaleConfiguration('nl-NL', 'nl')
@@ -156,7 +156,7 @@ server {
             configuration = Configuration(
                 output_directory_path, 'http://example.com')
             configuration.content_negotiation = True
-            configuration.extensions[Nginx] = {}
+            configuration.extensions[Nginx] = Nginx.configuration_schema({})
             configuration.locales.clear()
             configuration.locales['en-US'] = LocaleConfiguration('en-US', 'en')
             configuration.locales['nl-NL'] = LocaleConfiguration('nl-NL', 'nl')
@@ -220,7 +220,7 @@ server {
             configuration = Configuration(
                 output_directory_path, 'http://example.com')
             configuration.content_negotiation = True
-            configuration.extensions[Nginx] = {}
+            configuration.extensions[Nginx] = Nginx.configuration_schema({})
             expected = r'''
 server {
     listen 80;
@@ -261,7 +261,7 @@ server {
         with TemporaryDirectory() as output_directory_path:
             configuration = Configuration(
                 output_directory_path, 'https://example.com')
-            configuration.extensions[Nginx] = {}
+            configuration.extensions[Nginx] = Nginx.configuration_schema({})
             expected = r'''
 server {
     listen 80;
@@ -302,9 +302,9 @@ server {
         with TemporaryDirectory() as output_directory_path:
             configuration = Configuration(
                 output_directory_path, 'https://example.com')
-            configuration.extensions[Nginx] = {
+            configuration.extensions[Nginx] = Nginx.configuration_schema({
                 'www_directory_path': '/tmp/overridden-www'
-            }
+            })
             expected = r'''
 server {
     listen 80;
