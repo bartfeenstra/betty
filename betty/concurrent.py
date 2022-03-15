473c023f53c808454bf2f78563b20f089e746b2c
Bart Feenstra <bart@mynameisbart.com>
2022-03-15 15:35:55 +0000

    Improve test coverage of the exception-raising awaitable executor.

--- betty/concurrent.py
+++ betty/concurrent.py
@@ -4,20 +4,22 @@ from concurrent.futures._base import Executor, wait
 class ExceptionRaisingAwaitableExecutor(Executor):
     def __init__(self, executor: Executor):
         self._executor = executor
-        self._futures = []
+        self._awaitables = []
 
     def submit(self, *args, **kwargs):
         future = self._executor.submit(*args, **kwargs)
-        self._futures.append(future)
+        self._awaitables.append(future)
         return future
 
     def map(self, *args, **kwargs):
         return self._executor.map(*args, **kwargs)
 
     def wait(self) -> None:
-        wait(self._futures)
+        awaitables = self._awaitables
+        self._awaitables = []
+        wait(awaitables)
 
     def shutdown(self, *args, **kwargs):
         self._executor.shutdown(*args, **kwargs)
-        for future in self._futures:
+        for future in self._awaitables:
             future.result()
