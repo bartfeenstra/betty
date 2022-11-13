56573f0038429f8b0d2fca548e65595d85a8671d
Bart Feenstra <bart@mynameisbart.com>
2022-11-13 01:09:17 +0000

    Make the Cotton Candy colors configurable. (#910)

--- betty/cotton_candy/__init__.py
+++ betty/cotton_candy/__init__.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from typing import Optional, TYPE_CHECKING, Set, Type, Dict, Callable
 
@@ -19,15 +20,77 @@ if TYPE_CHECKING:
     from betty.builtins import _
 
 
+class _ColorConfiguration(Configuration):
+    _HEX_PATTERN = re.compile(r'^#[a-zA-Z0-9]{6}$')
+
+    def __init__(self, hex_value: str):
+        super().__init__()
+        self._hex: str
+        self.hex = hex_value
+
+    @reactive  # type: ignore
+    @property
+    def hex(self) -> str:
+        return self._hex
+
+    @hex.setter
+    def hex(self, hex_value: str) -> None:
+        if hex_value is None:
+            self._hex = None
+            return
+
+        if not self._HEX_PATTERN.match(hex_value):
+            raise ConfigurationError(_('"{hex_value}" is not a valid hexadecimal color, such as #ffc0cb.').format(hex_value=hex_value))
+
+        self._hex = hex_value
+
+    def load(self, dumped_configuration: DumpedConfiguration) -> None:
+        if not isinstance(dumped_configuration, str):
+            raise ConfigurationError(_('Color configuration must be a string.'))
+        self.hex = dumped_configuration
+
+    def dump(self) -> DumpedConfiguration:
+        return self._hex
+
+
 class CottonCandyConfiguration(Configuration):
+    DEFAULT_PRIMARY_INACTIVE_COLOR = '#ffc0cb'
+    DEFAULT_PRIMARY_ACTIVE_COLOR = '#ff69b4'
+    DEFAULT_LINK_INACTIVE_COLOR = '#149988'
+    DEFAULT_LINK_ACTIVE_COLOR = '#2a615a'
+
     def __init__(self):
         super().__init__()
         self._featured_entities = EntityReferences()
         self._featured_entities.react(self)
+        self._primary_inactive_color = _ColorConfiguration(self.DEFAULT_PRIMARY_INACTIVE_COLOR)
+        self._primary_inactive_color.react(self)
+        self._primary_active_color = _ColorConfiguration(self.DEFAULT_PRIMARY_ACTIVE_COLOR)
+        self._primary_active_color.react(self)
+        self._link_inactive_color = _ColorConfiguration(self.DEFAULT_LINK_INACTIVE_COLOR)
+        self._link_inactive_color.react(self)
+        self._link_active_color = _ColorConfiguration(self.DEFAULT_LINK_ACTIVE_COLOR)
+        self._link_active_color.react(self)
 
     @property
     def featured_entities(self) -> EntityReferences:
         return self._featured_entities
+
+    @property
+    def primary_inactive_color(self) -> _ColorConfiguration:
+        return self._primary_inactive_color
+
+    @property
+    def primary_active_color(self) -> _ColorConfiguration:
+        return self._primary_active_color
+
+    @property
+    def link_inactive_color(self) -> _ColorConfiguration:
+        return self._link_inactive_color
+
+    @property
+    def link_active_color(self) -> _ColorConfiguration:
+        return self._link_active_color
 
     def load(self, dumped_configuration: DumpedConfiguration) -> None:
         if not isinstance(dumped_configuration, dict):
@@ -35,11 +98,31 @@ class CottonCandyConfiguration(Configuration):
 
         if 'featured_entities' in dumped_configuration:
             with ensure_context('featured_entities'):
-                self.featured_entities.load(dumped_configuration['featured_entities'])
+                self._featured_entities.load(dumped_configuration['featured_entities'])
+
+        if 'primary_inactive_color' in dumped_configuration:
+            with ensure_context('primary_inactive_color'):
+                self._primary_inactive_color.load(dumped_configuration['primary_inactive_color'])
+
+        if 'primary_active_color' in dumped_configuration:
+            with ensure_context('primary_active_color'):
+                self._primary_active_color.load(dumped_configuration['primary_active_color'])
+
+        if 'link_inactive_color' in dumped_configuration:
+            with ensure_context('link_inactive_color'):
+                self._link_inactive_color.load(dumped_configuration['link_inactive_color'])
+
+        if 'link_active_color' in dumped_configuration:
+            with ensure_context('link_active_color'):
+                self._link_active_color.load(dumped_configuration['link_active_color'])
 
     def dump(self) -> DumpedConfiguration:
         return minimize_dumped_configuration({
             'featured_entities': self.featured_entities.dump(),
+            'primary_inactive_color': self._primary_inactive_color.dump(),
+            'primary_active_color': self._primary_active_color.dump(),
+            'link_inactive_color': self._link_inactive_color.dump(),
+            'link_active_color': self._link_active_color.dump(),
         })
 
 
@@ -63,7 +146,7 @@ class CottonCandy(Theme, ConfigurableExtension[CottonCandyConfiguration], GuiBui
 
     @classmethod
     def description(cls) -> str:
-        return _('Cotton Candy is a light theme featuring pastel colors')
+        return _("Cotton Candy is Betty's default theme.")
 
     def gui_build(self) -> QWidget:
         from betty.cotton_candy.gui import _CottonCandyGuiWidget
--- betty/tests/cotton_candy/test___init__.py
+++ betty/tests/cotton_candy/test___init__.py
@@ -3,11 +3,51 @@ from typing import Dict
 import pytest
 
 from betty.app import App
-from betty.config import ConfigurationError
-from betty.cotton_candy import CottonCandyConfiguration
+from betty.config import ConfigurationError, DumpedConfiguration
+from betty.cotton_candy import CottonCandyConfiguration, _ColorConfiguration
 from betty.model import Entity, get_entity_type_name
 from betty.project import EntityReference
-from betty.typing import Void
+
+
+class TestColorConfiguration:
+    def test_hex_with_valid_value(self) -> None:
+        hex_value = '#000000'
+        sut = _ColorConfiguration('#ffffff')
+        sut.hex = hex_value
+        assert hex_value == sut.hex
+
+    @pytest.mark.parametrize('hex_value', [
+        'rgb(0,0,0)',
+        'pink',
+    ])
+    def test_hex_with_invalid_value(self, hex_value: str) -> None:
+        sut = _ColorConfiguration('#ffffff')
+        with App():
+            with pytest.raises(ConfigurationError):
+                sut.hex = hex_value
+
+    def test_load_with_valid_hex_value(self) -> None:
+        hex_value = '#000000'
+        dumped_configuration = hex_value
+        sut = _ColorConfiguration('#ffffff')
+        sut.load(dumped_configuration)
+        assert hex_value == sut.hex
+
+    @pytest.mark.parametrize('dumped_configuration', [
+        False,
+        123,
+        'rgb(0,0,0)',
+        'pink',
+    ])
+    def test_load_with_invalid_value(self, dumped_configuration: DumpedConfiguration) -> None:
+        sut = _ColorConfiguration('#ffffff')
+        with App():
+            with pytest.raises(ConfigurationError):
+                sut.load(dumped_configuration)
+
+    def test_dump_with_value(self) -> None:
+        hex_value = '#000000'
+        assert hex_value == _ColorConfiguration(hex_value=hex_value).dump()
 
 
 class TestCottonCandyConfiguration:
@@ -22,17 +62,11 @@ class TestCottonCandyConfiguration:
             with pytest.raises(ConfigurationError):
                 CottonCandyConfiguration().load(dumped_configuration)
 
-    def test_dump_with_minimal_configuration(self) -> None:
-        sut = CottonCandyConfiguration()
-        expected = Void
-        assert expected == sut.dump()
-
-    def test_dump_with_featured_entities(self) -> None:
+    def test_load_with_featured_entities(self) -> None:
         sut = CottonCandyConfiguration()
         entity_type = Entity
         entity_id = '123'
-        sut.featured_entities.append(EntityReference(entity_type, entity_id))
-        expected = {
+        dumped_configuration = {
             'featured_entities': [
                 {
                     'entity_type': get_entity_type_name(entity_type),
@@ -40,4 +74,89 @@ class TestCottonCandyConfiguration:
                 },
             ],
         }
+        sut.load(dumped_configuration)
+        assert entity_type == sut.featured_entities[0].entity_type
+        assert entity_id == sut.featured_entities[0].entity_id
+
+    def test_load_with_primary_inactive_color(self) -> None:
+        hex_value = '#000000'
+        dumped_configuration = {
+            'primary_inactive_color': hex_value,
+        }
+        sut = CottonCandyConfiguration()
+        sut.load(dumped_configuration)
+        assert hex_value == sut.primary_inactive_color.hex
+
+    def test_load_with_primary_active_color(self) -> None:
+        hex_value = '#000000'
+        dumped_configuration = {
+            'primary_active_color': hex_value,
+        }
+        sut = CottonCandyConfiguration()
+        sut.load(dumped_configuration)
+        assert hex_value == sut.primary_active_color.hex
+
+    def test_load_with_link_inactive_color(self) -> None:
+        hex_value = '#000000'
+        dumped_configuration = {
+            'link_inactive_color': hex_value,
+        }
+        sut = CottonCandyConfiguration()
+        sut.load(dumped_configuration)
+        assert hex_value == sut.link_inactive_color.hex
+
+    def test_load_with_link_active_color(self) -> None:
+        hex_value = '#000000'
+        dumped_configuration = {
+            'link_active_color': hex_value,
+        }
+        sut = CottonCandyConfiguration()
+        sut.load(dumped_configuration)
+        assert hex_value == sut.link_active_color.hex
+
+    def test_dump_with_minimal_configuration(self) -> None:
+        sut = CottonCandyConfiguration()
+        expected = {
+            'primary_inactive_color': CottonCandyConfiguration.DEFAULT_PRIMARY_INACTIVE_COLOR,
+            'primary_active_color': CottonCandyConfiguration.DEFAULT_PRIMARY_ACTIVE_COLOR,
+            'link_inactive_color': CottonCandyConfiguration.DEFAULT_LINK_INACTIVE_COLOR,
+            'link_active_color': CottonCandyConfiguration.DEFAULT_LINK_ACTIVE_COLOR,
+        }
         assert expected == sut.dump()
+
+    def test_dump_with_featured_entities(self) -> None:
+        sut = CottonCandyConfiguration()
+        entity_type = Entity
+        entity_id = '123'
+        sut.featured_entities.append(EntityReference(entity_type, entity_id))
+        expected = [
+            {
+                'entity_type': get_entity_type_name(entity_type),
+                'entity_id': entity_id,
+            },
+        ]
+        assert expected == sut.dump()['featured_entities']  # type: ignore
+
+    def test_dump_with_primary_inactive_color(self) -> None:
+        hex_value = '#000000'
+        sut = CottonCandyConfiguration()
+        sut.primary_inactive_color.hex = hex_value
+        assert hex_value == sut.dump()['primary_inactive_color']  # type: ignore
+
+    def test_dump_with_primary_active_color(self) -> None:
+        hex_value = '#000000'
+        sut = CottonCandyConfiguration()
+        sut.primary_active_color.hex = hex_value
+        assert hex_value == sut.dump()['primary_active_color']  # type: ignore
+
+    def test_dump_with_link_inactive_color(self) -> None:
+        hex_value = '#000000'
+        sut = CottonCandyConfiguration()
+        sut.link_inactive_color.hex = hex_value
+        assert hex_value == sut.dump()['link_inactive_color']  # type: ignore
+
+    def test_dump_with_link_active_color(self) -> None:
+        hex_value = '#000000'
+        sut = CottonCandyConfiguration()
+        sut.link_active_color.hex = hex_value
+        assert hex_value == sut.dump()['link_active_color']  # type: ignore
