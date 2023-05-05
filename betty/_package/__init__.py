9778f413172c03bc95982bb65f50686f6ddcdea2
Bart Feenstra <bart@mynameisbart.com>
2023-05-05 20:35:09 +0100

    Fix package metadata and data files. (#968)

--- betty/_package/__init__.py
+++ betty/_package/__init__.py
@@ -1,14 +1,39 @@
 """Provide tools for the various package managers Betty integrates with."""
 from glob import glob
 from pathlib import Path
-from typing import List
+from setuptools import find_packages as find_packages_setuptools
+from typing import List, Dict, Iterator
 
 from betty import _ROOT_DIRECTORY_PATH as ROOT_DIRECTORY_PATH
+from betty.os import ChDir
 
 
-def get_data_paths() -> List[Path]:
-    return [data_path for data_path in [
-        ROOT_DIRECTORY_PATH / 'VERSION',
-        *map(Path, glob(str(ROOT_DIRECTORY_PATH / 'betty' / 'assets' / '**'), recursive=True)),
-        *map(Path, glob(str(ROOT_DIRECTORY_PATH / 'betty' / '*' / 'assets' / '**'), recursive=True)),
-    ] if data_path.is_file()]
+def find_packages() -> List[str]:
+    return find_packages_setuptools(
+        '.',
+        exclude=[
+            'betty._package',
+            'betty._package.*',
+            'betty.tests',
+            'betty.tests.*',
+        ],
+    )
+
+
+def is_data_file(file_path: Path) -> bool:
+    if not (ROOT_DIRECTORY_PATH / 'betty' / file_path).is_file():
+        return False
+    if '__pycache__' in str(file_path):
+        return False
+    return True
+
+
+def get_data_paths() -> Dict[str, Iterator[Path]]:
+    with ChDir(f'{ROOT_DIRECTORY_PATH}/betty'):
+        return {
+            'betty': filter(is_data_file, [
+                Path('py.typed'),
+                *map(Path, glob('assets/**', recursive=True)),
+                *map(Path, glob('*/assets/**', recursive=True)),
+            ]),
+        }
