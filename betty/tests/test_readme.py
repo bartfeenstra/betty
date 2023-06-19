5698efe68fcb4cee1748d3ece72dcd22ea4c873f
Bart Feenstra <bart@mynameisbart.com>
2023-06-19 17:54:15 +0100

    Fix an invalid project configuration example in the README. (#998)

--- betty/tests/test_readme.py
+++ betty/tests/test_readme.py
@@ -1,9 +1,13 @@
 import json
+import re
 import subprocess as stdsubprocess
 import sys
 from asyncio import StreamReader
 
+import yaml
+
 from betty import os, subprocess
+from betty.project import ProjectConfiguration
 from betty.tempfile import TemporaryDirectory
 
 
@@ -25,3 +29,12 @@ class TestReadme:
             with open('README.md') as f:
                 actual = f.read()
             assert expected in actual
+
+    async def test_readme_should_contain_valid_configuration(self) -> None:
+        with open('README.md') as f:
+            readme = f.read()
+        match = re.search(r'^```yaml\nbase_url((.|\n)+?)```$', readme, re.MULTILINE)
+        assert match is not None
+        raw_dump = match[0][7:-3]
+        configuration = ProjectConfiguration()
+        configuration.load(yaml.safe_load(raw_dump))
