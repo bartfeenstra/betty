63376ae3581a7763fdd688b70d3ae12c5d998894
Bart Feenstra <bart@mynameisbart.com>
2024-01-18 19:34:10 +0000

    Fix Leaflet.js markers not showing due to incorrect URLs. (#1196)

--- betty/serve.py
+++ betty/serve.py
@@ -11,9 +11,10 @@ from http.server import SimpleHTTPRequestHandler, HTTPServer
 from io import StringIO
 from pathlib import Path
 from types import TracebackType
-from typing import Sequence
+from typing import Sequence, Any
 
-from aiofiles.os import makedirs
+from aiofiles.os import makedirs, symlink
+from aiofiles.tempfile import TemporaryDirectory, AiofilesContextManagerTempDir
 
 from betty.app import App
 from betty.asyncio import sync
@@ -119,13 +120,16 @@ class BuiltinServer(Server):
         self,
         www_directory_path: Path,
         *,
+        root_path: str | None = None,
         localizer: Localizer,
     ) -> None:
         super().__init__(localizer)
         self._www_directory_path = www_directory_path
+        self._root_path = root_path
         self._http_server: HTTPServer | None = None
         self._port: int | None = None
         self._thread: threading.Thread | None = None
+        self._temporary_root_directory: AiofilesContextManagerTempDir[None, Any, Any] | None = None
 
     @classmethod
     def label(cls) -> Str:
@@ -133,6 +137,19 @@ class BuiltinServer(Server):
 
     async def start(self) -> None:
         await super().start()
+        if self._root_path:
+            # To mimic the root path, symlink the project's WWW directory into a temporary
+            # directory, so we do not have to make changes to any existing files.
+            self._temporary_root_directory = TemporaryDirectory()
+            temporary_root_directory_path = Path(await self._temporary_root_directory.__aenter__())
+            temporary_www_directory = temporary_root_directory_path
+            for root_path_component in self._root_path.split('/'):
+                temporary_www_directory /= root_path_component
+            if temporary_www_directory != temporary_root_directory_path:
+                await symlink(self._www_directory_path, temporary_www_directory)
+            www_directory_path = temporary_root_directory_path
+        else:
+            www_directory_path = self._www_directory_path
         logging.getLogger(__name__).info(self._localizer._("Starting Python's built-in web server..."))
         for self._port in range(DEFAULT_PORT, 65535):
             with contextlib.suppress(OSError):
@@ -142,7 +159,7 @@ class BuiltinServer(Server):
                         request,
                         client_address,
                         server,
-                        directory=str(self._www_directory_path),
+                        directory=str(www_directory_path),
                     ),
                 )
                 break
@@ -154,7 +171,7 @@ class BuiltinServer(Server):
     @property
     def public_url(self) -> str:
         if self._port is not None:
-            return f'http://localhost:{self._port}'
+            return f'http://localhost:{self._port}/{self._root_path}'
         raise NoPublicUrlBecauseServerNotStartedError()
 
     @sync
@@ -170,6 +187,8 @@ class BuiltinServer(Server):
             self._http_server.server_close()
         if self._thread is not None:
             self._thread.join()
+        if self._temporary_root_directory is not None:
+            await self._temporary_root_directory.__aexit__(None, None, None)
 
 
 class BuiltinAppServer(AppServer):
@@ -177,6 +196,7 @@ class BuiltinAppServer(AppServer):
         super().__init__(app)
         self._server = BuiltinServer(
             self._app.project.configuration.www_directory_path,
+            root_path=self._app.project.configuration.root_path,
             localizer=self._app.localizer
         )
 
