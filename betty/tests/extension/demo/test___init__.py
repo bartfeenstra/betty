b8cd374a4e3397b93374886e6de6775d14ff164c
Bart Feenstra <bart@mynameisbart.com>
2024-03-23 00:18:51 +0000

    Reorganize CLI tests and improve coverage (#1340)

--- betty/tests/extension/demo/test___init__.py
+++ betty/tests/extension/demo/test___init__.py
@@ -1,7 +1,11 @@
+import requests
 from pytest_mock import MockerFixture
+from requests import Response
 
 from betty.app import App
 from betty.extension import Demo
+from betty.extension.demo import DemoServer
+from betty.functools import Do
 from betty.load import load
 from betty.model.ancestry import Person, Place, Event, Source, Citation
 from betty.project import ExtensionConfiguration
@@ -18,3 +22,13 @@ class TestDemo:
         assert 0 != len(app.project.ancestry[Event])
         assert 0 != len(app.project.ancestry[Source])
         assert 0 != len(app.project.ancestry[Citation])
+
+
+class TestDemoServer:
+    async def test(self, mocker: MockerFixture) -> None:
+        mocker.patch('webbrowser.open_new_tab')
+        async with DemoServer() as server:
+            def _assert_response(response: Response) -> None:
+                assert response.status_code == 200
+                assert 'Betty' in response.content.decode('utf-8')
+            await Do(requests.get, server.public_url).until(_assert_response)
