ca1b92931c5eaa93ab0241e5876be74d65561bf6
Bart Feenstra <bart@mynameisbart.com>
2024-04-16 23:10:49 +0100

    Convert the show/hide functionality to Typescript

--- betty/extension/cotton_candy/assets/betty.extension.npm._Npm/src/main.ts
+++ betty/extension/cotton_candy/assets/betty.extension.npm._Npm/src/main.ts
@@ -2,5 +2,5 @@
 
 import './file.js' // eslint-disable-line no-unused-vars
 import './search.ts' // eslint-disable-line no-unused-vars
-import './show.js' // eslint-disable-line no-unused-vars
+import './show.ts' // eslint-disable-line no-unused-vars
 import './main.scss' // eslint-disable-line @typescript-eslint/no-unused-vars
