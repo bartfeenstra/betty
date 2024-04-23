82605bfa5fd727c1487568ceac3315e49079c78b
Bart Feenstra <bart@mynameisbart.com>
2024-04-23 23:31:24 +0100

    Refactor the testing web server used in Playwright tests no longer to (#1406)
    require dependencies.

--- playwright/tests/index.ts
+++ playwright/tests/index.ts
@@ -1,11 +1,12 @@
+'use strict'
+
 import { test as base } from '@playwright/test'
-import { mkdtemp, writeFile } from 'node:fs/promises'
+import { access, mkdtemp, writeFile } from 'node:fs/promises'
+import { createServer, IncomingMessage, Server as HttpServer, ServerResponse } from 'node:http'
 import * as path from 'node:path'
 import { tmpdir } from 'node:os'
-import { createServer, Server as HttpServer } from 'node:http'
-import serveStatic = require('serve-static');
-import connect = require('connect');
 import { exec as childProcessExec } from 'node:child_process'
+import { createReadStream, ReadStream } from 'node:fs'
 
 async function sleep (milliseconds: number): Promise<unknown> {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
@@ -13,36 +14,65 @@ async function sleep (milliseconds: number): Promise<unknown> {
 
 function exec (command: string, options: object): Promise<string | Buffer> {
   return new Promise((resolve, reject) => {
-    childProcessExec(command, options, (error, stdout, stderr) => {
+    childProcessExec(command, options, (error, stdout) => {
       if (error === null) {
         resolve(stdout)
       } else {
-        reject(stderr)
+        reject(error)
       }
     })
   })
 }
 
-async function buildApp (projectDirectoryPath: string, bettyConfiguration: object): Promise<string> {
+async function buildApp (projectDirectoryPath: string, bettyConfiguration: object): Promise<void> {
   // We do not know the real base URL, but as Betty requires one, set an obviously fake value.
   bettyConfiguration.base_url = 'https://example.com'
   await writeFile(path.join(projectDirectoryPath, 'betty.json'), JSON.stringify(bettyConfiguration))
   await exec('betty generate', {
     cwd: projectDirectoryPath
   })
-  return projectDirectoryPath
+}
+
+interface ServerResponseMeta {
+  contentType: string
+  content: ReadStream
+  code: number
 }
 
 class Server implements Disposable {
-  private readonly host = '127.0.0.1'
-  private httpServer: HttpServer
-  private portNumber: number | null = null
   private attemptedPortNumber = 8000
+  private readonly contentTypes: Record<string, string> = {
+    default: 'application/octet-stream',
+    html: 'text/html; charset=UTF-8',
+    js: 'application/javascript',
+    css: 'text/css',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    gif: 'image/gif',
+    ico: 'image/x-icon',
+    svg: 'image/svg+xml'
+  }
 
-  constructor (wwwDirectoryPath: string) {
-    const app = connect()
-      .use(serveStatic(wwwDirectoryPath))
-    this.httpServer = createServer(app)
+  private readonly host = '127.0.0.1'
+  private readonly httpServer: HttpServer
+  private portNumber: number | null = null
+  private readonly wwwDirectoryPath: string
+
+  public constructor (wwwDirectoryPath: string) {
+    this.wwwDirectoryPath = wwwDirectoryPath
+
+    this.httpServer = createServer(
+      async (request: IncomingMessage, response: ServerResponse) => { // eslint-disable-line @typescript-eslint/no-misused-promises
+        const responseMeta = await this.prepareFile(request.url)
+        response.writeHead(
+          responseMeta.code,
+          {
+            'Content-Type': responseMeta.contentType
+          }
+        )
+        responseMeta.content.pipe(response)
+      }
+    )
     this.httpServer.on('error', (error) => {
       if (error.code === 'EADDRINUSE') {
         this.httpServer.close()
@@ -54,21 +84,55 @@ class Server implements Disposable {
     this.listen()
   }
 
-  private listen (): void {
-    this.httpServer.listen(++this.attemptedPortNumber, this.host, () => {
-      this.portNumber = this.attemptedPortNumber
-    })
+  private async prepareFile (urlPath: string): Promise<ServerResponseMeta> {
+    // @todo Remove this favicon workaround. See https://github.com/bartfeenstra/betty/issues/1404.
+    if (urlPath === '/favicon.ico') {
+      urlPath = '/betty.ico'
+    }
+    const urlPathComponents = [this.wwwDirectoryPath, urlPath]
+    if (urlPath.endsWith('/')) {
+      urlPathComponents.push('index.html')
+    }
+    const requestFilePath = path.join(...urlPathComponents)
+    let requestFileFound: boolean
+    try {
+      await access(requestFilePath)
+      requestFileFound = true
+    } catch {
+      requestFileFound = false
+    }
+    const responseStreamPath = requestFileFound ? requestFilePath : this.wwwDirectoryPath + '/.error/404.html'
+    const fileExtension = path.extname(responseStreamPath).substring(1).toLowerCase()
+    return {
+      code: requestFileFound ? 200 : 404,
+      content: createReadStream(responseStreamPath),
+      contentType: this.contentTypes[fileExtension] || this.contentTypes.default
+    }
   }
 
   public async getPublicUrl (): Promise<string> {
     while (this.portNumber === null) {
       await sleep(10)
     }
-    return `http://${this.host}:${this.portNumber}`
+    return `http://${this.host}:${this.portNumber.toString()}`
   }
 
-  public async [Symbol.dispose] () {
+  private listen (): void {
+    this.httpServer.listen(
+      ++this.attemptedPortNumber,
+      this.host,
+      () => {
+        this.portNumber = this.attemptedPortNumber
+      }
+    )
+  }
+
+  public close (): void {
     this.httpServer.close()
+  }
+
+  public [Symbol.dispose] (): void {
+    this.close()
   }
 }
 
@@ -83,7 +147,7 @@ const test = base.extend<{
   }
 })
 
-module.exports = {
+export {
   buildApp,
   Server,
   test
