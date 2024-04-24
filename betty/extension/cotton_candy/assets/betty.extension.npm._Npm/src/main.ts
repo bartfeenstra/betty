f2cad6b5b842ca2698c20f0e5ffb24d0459e44fd
Bart Feenstra <bart@mynameisbart.com>
2024-04-24 10:57:51 +0100

    Convert the Cotton Candy JavaScript entrypoint to TypeScript

--- betty/extension/cotton_candy/assets/betty.extension.npm._Npm/src/main.ts
+++ betty/extension/cotton_candy/assets/betty.extension.npm._Npm/src/main.ts
@@ -3,4 +3,4 @@
 import './file.js' // eslint-disable-line no-unused-vars
 import './search.ts' // eslint-disable-line no-unused-vars
 import './show.js' // eslint-disable-line no-unused-vars
-import mainStyle from './main.scss' // eslint-disable-line no-unused-vars
+import './main.scss' // eslint-disable-line @typescript-eslint/no-unused-vars
