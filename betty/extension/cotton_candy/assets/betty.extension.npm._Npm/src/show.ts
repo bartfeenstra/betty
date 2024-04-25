cc16a4d6275862f0bea33571529d14a780ac61b0
Bart Feenstra <bart@mynameisbart.com>
2024-04-25 16:56:04 +0100

    Convert the show/hide functionality to Typescript (#1400)

---
+++ betty/extension/cotton_candy/assets/betty.extension.npm._Npm/src/show.ts
@@ -0,0 +1,30 @@
+'use strict'
+
+function initializeToggles () :void {
+  const toggles = document.getElementsByClassName('show-toggle')
+  for (const toggle of toggles) {
+    initializeToggle(toggle as Element)
+  }
+}
+
+function initializeToggle (toggle: Element) :void {
+  toggle.addEventListener('click', (event: PointerEvent) => {
+    const container: Element | null = getContainer(event.target as Element) // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+    if (container) {
+      container.classList.toggle('show-shown')
+    }
+  })
+}
+
+function getContainer (node: Element): Element | null {
+  const parentNode = node.parentNode as Element | null
+  if (!parentNode) {
+    return
+  }
+  if (parentNode.classList.contains('show')) {
+    return parentNode
+  }
+  return getContainer(parentNode)
+}
+
+document.addEventListener('DOMContentLoaded', initializeToggles)
