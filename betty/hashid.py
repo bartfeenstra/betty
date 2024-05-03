915534fb450c588282a7a08611d75a376c16cc64
Bart Feenstra <bart@mynameisbart.com>
2024-05-03 17:15:19 +0100

    Improve the uniqueness of the new hashid API

--- betty/hashid.py
+++ betty/hashid.py
@@ -5,10 +5,10 @@ This module MUST NOT be used for security purposes.
 """
 
 from hashlib import md5
-from os.path import getmtime
 from pathlib import Path
 
 import aiofiles
+from aiofiles.os import stat
 
 
 def _hashid_bytes(key: bytes) -> str:
@@ -35,17 +35,24 @@ async def hashid_file_meta(file_path: Path) -> str:
     """
     Create a hash ID for a file based on its metadata.
 
-    This function relies on the file path and last modified time for uniqueness.
+    This function relies on the file path, size, and last modified time for uniqueness.
     File contents are ignored. This may be suitable for large files whose
     exact contents may not be very relevant in the context the ID is used in.
     """
-    return hashid(f"{getmtime(file_path)}:{file_path}")
+    file_stat_result = await stat(file_path)
+    return hashid_sequence(
+        str(file_path), str(file_stat_result.st_size), str(file_stat_result.st_mtime_ns)
+    )
 
 
 async def hashid_file_content(file_path: Path) -> str:
     """
     Create a hash ID for a file based on its contents.
+
+    This function relies on the file path and contents for uniqueness.
+    File contents must be loaded into memory in their entirety, which is why
+    :py:func:`betty.hashid.hashid_file_meta` may be more suitable for large files.
     """
     async with aiofiles.open(file_path, "rb") as f:
         file_content = await f.read()
-    return _hashid_bytes(file_content)
+    return hashid_sequence(str(file_path), file_content)
--- betty/tests/test_hashid.py
+++ betty/tests/test_hashid.py
@@ -4,13 +4,13 @@ import aiofiles
 
 from betty.hashid import hashid_file_content, hashid_file_meta, hashid, hashid_sequence
 
+
 content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-content_hash = "35899082e51edf667f14477ac000cbba"
 
 
 class TestHashid:
     async def test(self) -> None:
-        assert hashid(content) == content_hash
+        assert hashid(content) == "35899082e51edf667f14477ac000cbba"
 
 
 class TestHashidSequence:
@@ -40,4 +40,15 @@ class TestHashidFileContent:
         file_path = tmp_path / "file"
         async with aiofiles.open(file_path, "w") as f:
             await f.write(content)
-        assert await hashid_file_content(file_path) == content_hash
+        assert await hashid_file_content(file_path) == await hashid_file_content(
+            file_path
+        )
+
+    async def test_with_different_files(self, tmp_path: Path) -> None:
+        file_left_path = tmp_path / "file-left"
+        file_right_path = tmp_path / "file-right"
+        file_left_path.touch()
+        file_right_path.touch()
+        assert await hashid_file_content(file_left_path) != await hashid_file_meta(
+            file_right_path
+        )
