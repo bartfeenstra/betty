5376e3cc540183ce9dc087bb353b8d94b1f01b70
Bart Feenstra <bart@mynameisbart.com>
2024-05-11 13:17:11 +0100

    Make TestTestsExist read .coveragerc and use the ignore rules in there (#1490)

--- betty/tests/test_tests_exist.py
+++ betty/tests/test_tests_exist.py
@@ -1,5 +1,8 @@
 from _ast import Expr, Constant
 from ast import parse, iter_child_nodes
+from collections.abc import Sequence, Iterator
+from configparser import ConfigParser
+from glob import glob
 from pathlib import Path
 
 import aiofiles
@@ -8,47 +11,41 @@ from betty.fs import iterfiles, ROOT_DIRECTORY_PATH
 
 
 class TestTestsExist:
-    # These modules are known to have no coverage, and form the baseline this test
-    # operates on. This baseline MUST NOT be extended. It SHOULD decrease in size
-    # as more coverage is added to Betty over time.
-    _BASELINE_MODULES = {
-        "betty.__init__",
-        "betty._package.__init__",
-        "betty._package.pyinstaller.__init__",
-        "betty._package.pyinstaller.hooks.__init__",
-        "betty._package.pyinstaller.hooks.hook-betty",
-        "betty._package.pyinstaller.main",
-        "betty._patch",
-        "betty._resizeimage",
-        "betty.app.extension.requirement",
-        "betty.cache._base",
-        "betty.classtools",
-        "betty.dispatch",
-        "betty.error",
-        "betty.extension.__init__",
-        "betty.extension.nginx.docker",
-        "betty.extension.webpack.jinja2.__init__",
-        "betty.extension.webpack.jinja2.filter",
-        "betty.gramps.error",
-        "betty.gui.__init__",
-        "betty.gui.error",
-        "betty.gui.locale",
-        "betty.gui.logging",
-        "betty.gui.model",
-        "betty.gui.serve",
-        "betty.gui.text",
-        "betty.gui.window",
-        "betty.html",
-        "betty.jinja2.__init__",
-        "betty.jinja2.filter",
-        "betty.jinja2.test",
-        "betty.json.linked_data",
-        "betty.load",
-        "betty.path",
-        "betty.render",
-        "betty.serde.dump",
-        "betty.sphinx.extension.replacements",
-        "betty.warnings",
+    # These modules files are known to have no coverage, and form the baseline this
+    # test operates on. This baseline MUST NOT be extended. It SHOULD decrease in
+    # size as more coverage is added to Betty over time.
+    _BASELINE_BETTY_MODULE_FILES = {
+        "betty/__init__.py",
+        "betty/_patch.py",
+        "betty/app/extension/requirement.py",
+        "betty/cache/_base.py",
+        "betty/classtools.py",
+        "betty/dispatch.py",
+        "betty/error.py",
+        "betty/extension/__init__.py",
+        "betty/extension/nginx/docker.py",
+        "betty/extension/webpack/jinja2/__init__.py",
+        "betty/extension/webpack/jinja2/filter.py",
+        "betty/gramps/error.py",
+        "betty/gui/__init__.py",
+        "betty/gui/error.py",
+        "betty/gui/locale.py",
+        "betty/gui/logging.py",
+        "betty/gui/model.py",
+        "betty/gui/serve.py",
+        "betty/gui/text.py",
+        "betty/gui/window.py",
+        "betty/html.py",
+        "betty/jinja2/__init__.py",
+        "betty/jinja2/filter.py",
+        "betty/jinja2/test.py",
+        "betty/json/linked_data.py",
+        "betty/load.py",
+        "betty/path.py",
+        "betty/render.py",
+        "betty/serde/dump.py",
+        "betty/sphinx/extension/replacements.py",
+        "betty/warnings.py",
     }
 
     async def test(self) -> None:
@@ -56,17 +53,39 @@ class TestTestsExist:
             if file_path.suffix == ".py":
                 await self._test_python_file(file_path)
 
+    def _module_path_to_name(self, relative_module_path: Path) -> str:
+        module_name_parts = relative_module_path.parent.parts
+        if relative_module_path.name != "__init__.py":
+            module_name_parts = (*module_name_parts, relative_module_path.name[:-3])
+        return ".".join(module_name_parts)
+
+    def _get_coveragerc_ignore_modules(self) -> Iterator[Path]:
+        coveragerc = ConfigParser()
+        coveragerc.read(ROOT_DIRECTORY_PATH / ".coveragerc")
+        omit = coveragerc.get("run", "omit").split("\n")
+        for omit_pattern in omit:
+            for module_path_str in glob(omit_pattern, recursive=True):
+                if not module_path_str.endswith(".py"):
+                    continue
+                module_path = Path(module_path_str)
+                if not module_path.is_file():
+                    continue
+                yield module_path
+
+    async def _get_ignore_module_paths(self) -> Sequence[Path]:
+        return (
+            *map(Path, self._BASELINE_BETTY_MODULE_FILES),
+            *self._get_coveragerc_ignore_modules(),
+        )
+
     async def _test_python_file(self, file_path: Path) -> None:
         # Skip tests.
         if ROOT_DIRECTORY_PATH / "betty" / "tests" in file_path.parents:
             return
 
-        module_name = ".".join(
-            (
-                *file_path.relative_to(ROOT_DIRECTORY_PATH).parent.parts,
-                file_path.stem,
-            )
-        )
+        module_path = file_path.relative_to(ROOT_DIRECTORY_PATH)
+
+        ignore_module_paths = await self._get_ignore_module_paths()
 
         expected_test_file_path = (
             ROOT_DIRECTORY_PATH
@@ -76,23 +95,21 @@ class TestTestsExist:
             / f"test_{file_path.name}"
         )
         if expected_test_file_path.exists():
-            for baseline_module in self._BASELINE_MODULES:
-                if module_name == baseline_module or module_name.startswith(
-                    f"{baseline_module}."
-                ):
+            for ignore_module_path in ignore_module_paths:
+                if module_path == ignore_module_path:
                     raise AssertionError(
-                        f"Module {module_name} has a matching test file, but unexpectedly matches the baseline module {baseline_module}."
+                        f"{module_path} has a matching test file at {ignore_module_path}, but was unexpectedly configured to be ignored."
                     )
             return
 
-        if module_name in self._BASELINE_MODULES:
+        if module_path in ignore_module_paths:
             return
 
         if await self._test_python_module_contains_docstring_only(file_path):
             return
 
         raise AssertionError(
-            f"Module {module_name} does not have a matching test file. Expected {expected_test_file_path} to exist."
+            f"{module_path} does not have a matching test file. Expected {expected_test_file_path} to exist."
         )
 
     async def _test_python_module_contains_docstring_only(
