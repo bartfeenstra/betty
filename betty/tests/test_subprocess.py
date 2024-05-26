38a8cfcad058fa10cdf9d942b6cb00a31bddda93
Bart Feenstra <bart@mynameisbart.com>
2024-05-26 15:31:11 +0200

    Improve performance of subprocess pipes with large amounts of output

--- betty/tests/test_subprocess.py
+++ betty/tests/test_subprocess.py
@@ -1,7 +1,11 @@
+import logging
 from asyncio.subprocess import Process
+from pathlib import Path
 from subprocess import CalledProcessError
 
+import aiofiles
 import pytest
+from _pytest.logging import LogCaptureFixture
 
 from betty.subprocess import run_process
 
@@ -25,6 +29,26 @@ class TestRunProcess:
             False,
         ],
     )
-    async def test_with_errors(self, shell: bool) -> None:
-        with pytest.raises(CalledProcessError):
-            await run_process(["false"], shell=shell)
+    async def test_with_errors(
+        self, shell: bool, caplog: LogCaptureFixture, tmp_path: Path
+    ) -> None:
+        stdout_sentinel = "Hello, stdout!"
+        stderr_sentinel = "Hello, stderr!"
+        script_path = tmp_path / "test.py"
+        python_script = f"""
+import sys
+print("{stdout_sentinel}")
+print("{stderr_sentinel}", file=sys.stderr)
+sys.exit(1)"""
+        async with aiofiles.open(script_path, "w") as f:
+            await f.write(python_script)
+        with pytest.raises(CalledProcessError), caplog.at_level(logging.NOTSET):
+            await run_process(
+                [
+                    "python",
+                    str(script_path),
+                ],
+                shell=shell,
+            )
+        assert f"stdout:\n{stdout_sentinel}" in caplog.text
+        assert f"stderr:\n{stderr_sentinel}" in caplog.text
