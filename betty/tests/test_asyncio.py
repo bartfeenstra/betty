8b38ebd84e958afd270c58839d4ba3bf1290b543
Bart Feenstra <bart@mynameisbart.com>
2024-06-21 19:42:45 +0100

    Remove `asyncio.sync()` and `asyncio.wait()` (#1588)

--- betty/tests/test_asyncio.py
+++ betty/tests/test_asyncio.py
@@ -1,21 +1,4 @@
-from typing import Any
-
-import pytest
-
-from betty.asyncio import sync, wait, wait_to_thread
-from betty.warnings import BettyDeprecationWarning
-
-
-class TestWait:
-    async def test(self) -> None:
-        expected = "Hello, oh asynchronous, world!"
-
-        async def _async() -> str:
-            return expected
-
-        with pytest.warns(BettyDeprecationWarning):
-            actual = wait(_async())
-        assert expected == actual
+from betty.asyncio import wait_to_thread
 
 
 class TestWaitToThread:
@@ -27,63 +10,3 @@ class TestWaitToThread:
 
         actual = wait_to_thread(_async())
         assert expected == actual
-
-
-class TestSync:
-    async def test_call_decorated_coroutinefunction_should_return_result(self) -> None:
-        expected = "Hello, oh asynchronous, world!"
-
-        with pytest.warns(BettyDeprecationWarning):
-
-            @sync
-            async def _async() -> str:
-                return expected
-
-            actual = _async()
-        assert expected == actual
-
-    async def test_call_decorated_callable_coroutinemethod_should_return_result(
-        self,
-    ) -> None:
-        expected = "Hello, oh asynchronous, world!"
-
-        with pytest.warns(BettyDeprecationWarning):
-
-            class _Sync:
-                @sync  # type: ignore[callable-functiontype]
-                async def __call__(self, *args: Any, **kwargs: Any) -> str:
-                    return expected
-
-            actual = _Sync()()  # type: ignore[call-arg]
-        assert expected == actual
-
-    async def test_call_wrapped_coroutinecallable_object_should_return_result(
-        self,
-    ) -> None:
-        expected = "Hello, oh asynchronous, world!"
-
-        class _Sync:
-            async def __call__(self, *args: Any, **kwargs: Any) -> str:
-                return expected
-
-        with pytest.warns(BettyDeprecationWarning):
-            actual = sync(_Sync())()
-        assert expected == actual
-
-    async def test_call_nested_sync_and_async(self) -> None:
-        expected = "Hello, oh asynchronous, world!"
-
-        with pytest.warns(BettyDeprecationWarning):
-
-            @sync
-            async def _async_one() -> str:
-                return _sync()
-
-            def _sync() -> str:
-                return _async_two()
-
-            @sync
-            async def _async_two() -> str:
-                return expected
-
-            assert expected == _async_one()
--- pyinstaller.spec
+++ pyinstaller.spec
@@ -1,7 +1,7 @@
 # -*- mode: python ; coding: utf-8 -*-
 
 from betty._package.pyinstaller import a_pyz_exe_coll
-from betty.asyncio import wait
+from betty.asyncio import wait_to_thread
 
 
-a, pyz, exe, coll = wait(a_pyz_exe_coll())
+a, pyz, exe, coll = wait_to_thread(a_pyz_exe_coll())
