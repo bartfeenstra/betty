c2004cee0e5b7c27d111f48fb500d9018ff8db86
Bart Feenstra <bart@mynameisbart.com>
2024-06-21 22:12:07 +0100

    Remove the Job API's deprecated Context.claim()

--- betty/tests/test_job.py
+++ betty/tests/test_job.py
@@ -1,20 +1,9 @@
 from __future__ import annotations
 
-import pytest
-
 from betty.job import Context
-from betty.warnings import BettyDeprecationWarning
 
 
 class TestContext:
-    async def test_claim(self) -> None:
-        sut = Context()
-        job_ids = ("job ID 1", "job ID 2", "job ID 3")
-        for job_id in job_ids:
-            with pytest.warns(BettyDeprecationWarning):
-                assert sut.claim(job_id)
-                assert not sut.claim(job_id)
-
     async def test_start(self) -> None:
         sut = Context()
         sut.start  # noqa B018
