e57355a46eaf24b3e02937624ba30c44f298f7df
Bart Feenstra <bart@mynameisbart.com>
2024-07-09 00:11:27 +0100

    Make Dispatch API classes abstract and final (#1675)

--- betty/dispatch.py
+++ betty/dispatch.py
@@ -2,18 +2,20 @@
 Provide the Dispatch API.
 """
 
+from abc import ABC, abstractmethod
 from typing import Any, Sequence, Callable, Awaitable
 
 TargetedDispatcher = Callable[..., Awaitable[Sequence[Any]]]
 
 
-class Dispatcher:
+class Dispatcher(ABC):
     """
     Dispatch events to handlers.
     """
 
+    @abstractmethod
     def dispatch(self, target_type: type[Any]) -> TargetedDispatcher:
         """
         Dispatch a single target.
         """
-        raise NotImplementedError(repr(self))
+        pass
