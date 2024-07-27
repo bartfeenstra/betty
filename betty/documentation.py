248326475f096c0a723f0f9042319393410b9a3d
Bart Feenstra <bart@mynameisbart.com>
2024-07-27 23:01:45 +0100

    Remove the Sphinx link check, so websites being down does not disrupt Betty's builds (#1771)

--- betty/documentation.py
+++ betty/documentation.py
@@ -63,18 +63,6 @@ async def _build(output_directory_path: Path) -> None:
             [
                 "sphinx-build",
                 "-b",
-                "linkcheck",
-                "-j",
-                "auto",
-                str(source_directory_path),
-                str(output_directory_path),
-            ],
-            cwd=working_directory_path,
-        )
-        await run_process(
-            [
-                "sphinx-build",
-                "-b",
                 "dirhtml",
                 "-j",
                 "auto",
