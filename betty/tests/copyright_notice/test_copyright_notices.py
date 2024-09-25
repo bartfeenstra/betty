942de59eddd231e3a4e338d78bab81563fa5a32e
Bart Feenstra <bart@mynameisbart.com>
2024-09-25 08:25:52 +0100

    Add a copyright notice for works in the public domain (#2027)

--- betty/tests/copyright_notice/test_copyright_notices.py
+++ betty/tests/copyright_notice/test_copyright_notices.py
@@ -3,7 +3,7 @@ from typing import Sequence
 from typing_extensions import override
 
 from betty.copyright_notice import CopyrightNotice
-from betty.copyright_notice.copyright_notices import ProjectAuthor
+from betty.copyright_notice.copyright_notices import ProjectAuthor, PublicDomain
 from betty.locale.localizable import plain
 from betty.test_utils.copyright_notice import CopyrightNoticeTestBase
 
@@ -15,4 +15,19 @@ class TestProjectAuthor(CopyrightNoticeTestBase):
 
     @override
     def get_sut_instances(self) -> Sequence[CopyrightNotice]:
-        return [ProjectAuthor(None), ProjectAuthor(plain("My First Author"))]
+        return [
+            ProjectAuthor(None),
+            ProjectAuthor(plain("My First Author")),
+        ]
+
+
+class TestPublicDomain(CopyrightNoticeTestBase):
+    @override
+    def get_sut_class(self) -> type[CopyrightNotice]:
+        return PublicDomain
+
+    @override
+    def get_sut_instances(self) -> Sequence[CopyrightNotice]:
+        return [
+            PublicDomain(),
+        ]
