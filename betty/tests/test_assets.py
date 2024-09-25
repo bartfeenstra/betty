30c93d163b74e0c3dba9c91e24aaeca11855f81f
Bart Feenstra <bart@mynameisbart.com>
2024-09-25 14:34:57 +0100

    Fix a bug where the Assets API did not return the correct paths for files on disk (#2033)

--- betty/tests/test_assets.py
+++ betty/tests/test_assets.py
@@ -1,11 +1,38 @@
 from pathlib import Path
 
+import pytest
 from aiofiles.tempfile import TemporaryDirectory
 
 from betty.assets import AssetRepository
 
 
 class TestAssetRepository:
+    @pytest.fixture
+    async def sut(self, tmp_path: Path) -> tuple[AssetRepository, Path, Path]:
+        source_path_1 = tmp_path / "one"
+        source_path_1.mkdir()
+        (source_path_1 / "apples").touch()
+        (source_path_1 / "one").mkdir()
+        (source_path_1 / "one" / "oranges").touch()
+        (source_path_1 / "basket").mkdir()
+        (source_path_1 / "basket" / "tomatoes").touch()
+        (source_path_1 / "basket" / "aubergines").touch()
+
+        source_path_2 = tmp_path / "two"
+        source_path_2.mkdir()
+        (source_path_2 / "apples").touch()
+        (source_path_2 / "two").mkdir()
+        (source_path_2 / "two" / "oranges").touch()
+        (source_path_2 / "basket").mkdir()
+        (source_path_2 / "basket" / "tomatoes").touch()
+        (source_path_2 / "basket" / "courgettes").touch()
+
+        return (
+            AssetRepository(source_path_1, source_path_2),
+            source_path_1,
+            source_path_2,
+        )
+
     async def test_assets_directory_paths(self) -> None:
         async with TemporaryDirectory() as source_path_str_1:
             source_path_1 = Path(source_path_str_1)
@@ -14,86 +41,39 @@ class TestAssetRepository:
                 sut = AssetRepository(source_path_1, source_path_2)
                 assert sut.assets_directory_paths == (source_path_1, source_path_2)
 
-    async def test_get_with_override(
-        self,
+    async def test_get(self, sut: tuple[AssetRepository, Path, Path]) -> None:
+        sut, source_path_1, source_path_2 = sut
+        assert await sut.get(Path("apples")) == source_path_1 / "apples"
+        assert (
+            await sut.get(Path("one") / "oranges") == source_path_1 / "one" / "oranges"
+        )
+        assert (
+            await sut.get(Path("two") / "oranges") == source_path_2 / "two" / "oranges"
+        )
+
+    async def test_walk(self, sut: tuple[AssetRepository, Path, Path]) -> None:
+        sut, source_path_1, source_path_2 = sut
+        assert {path async for path in sut.walk()} == {
+            Path("apples"),
+            Path("basket") / "tomatoes",
+            Path("basket") / "aubergines",
+            Path("one") / "oranges",
+            Path("two") / "oranges",
+            Path("basket") / "courgettes",
+        }
+
+    async def test_walk_with_filter(
+        self, sut: tuple[AssetRepository, Path, Path]
     ) -> None:
-        async with TemporaryDirectory() as source_path_str_1:
-            source_path_1 = Path(source_path_str_1)
-            async with TemporaryDirectory() as source_path_str_2:
-                source_path_2 = Path(source_path_str_2)
-                (source_path_1 / "apples").touch()
-                (source_path_2 / "apples").touch()
-                sut = AssetRepository(source_path_1, source_path_2)
-                assert await sut.get(Path("apples")) == source_path_1 / "apples"
-
-    async def test_get_without_override(
-        self,
-    ) -> None:
-        async with TemporaryDirectory() as source_path_str_1:
-            source_path_1 = Path(source_path_str_1)
-            async with TemporaryDirectory() as source_path_str_2:
-                source_path_2 = Path(source_path_str_2)
-                (source_path_1 / "apples").touch()
-                (source_path_2 / "oranges").touch()
-                sut = AssetRepository(source_path_1, source_path_2)
-                assert await sut.get(Path("oranges")) == source_path_2 / "oranges"
-
-    async def test_walk_with_override(self) -> None:
-        async with TemporaryDirectory() as source_path_str_1:
-            source_path_1 = Path(source_path_str_1)
-            async with TemporaryDirectory() as source_path_str_2:
-                source_path_2 = Path(source_path_str_2)
-                (source_path_1 / "apples").touch()
-                (source_path_2 / "apples").touch()
-                sut = AssetRepository(source_path_1, source_path_2)
-                assert {path async for path in sut.walk()} == {Path("apples")}
-
-    async def test_walk_without_override(self) -> None:
-        async with TemporaryDirectory() as source_path_str_1:
-            source_path_1 = Path(source_path_str_1)
-            async with TemporaryDirectory() as source_path_str_2:
-                source_path_2 = Path(source_path_str_2)
-                (source_path_1 / "apples").touch()
-                (source_path_2 / "oranges").touch()
-                sut = AssetRepository(source_path_1, source_path_2)
-                assert {path async for path in sut.walk()} == {
-                    Path("apples"),
-                    Path("oranges"),
-                }
-
-    async def test_walk_with_override_with_filter(self) -> None:
-        async with TemporaryDirectory() as source_path_str_1:
-            source_path_1 = Path(source_path_str_1)
-            (source_path_1 / "fruits").mkdir()
-            (source_path_1 / "vegetables").mkdir()
-            async with TemporaryDirectory() as source_path_str_2:
-                source_path_2 = Path(source_path_str_2)
-                (source_path_2 / "fruits").mkdir()
-                (source_path_2 / "vegetables").mkdir()
-                (source_path_1 / "fruits" / "apples").touch()
-                (source_path_2 / "fruits" / "apples").touch()
-                (source_path_1 / "vegetables" / "peppers").touch()
-                (source_path_2 / "vegetables" / "peppers").touch()
-                sut = AssetRepository(source_path_1, source_path_2)
-                assert {path async for path in sut.walk(Path("fruits"))} == {
-                    Path("fruits") / "apples"
-                }
-
-    async def test_walk_without_override_with_filter(self) -> None:
-        async with TemporaryDirectory() as source_path_str_1:
-            source_path_1 = Path(source_path_str_1)
-            (source_path_1 / "fruits").mkdir()
-            (source_path_1 / "vegetables").mkdir()
-            async with TemporaryDirectory() as source_path_str_2:
-                source_path_2 = Path(source_path_str_2)
-                (source_path_2 / "fruits").mkdir()
-                (source_path_2 / "vegetables").mkdir()
-                (source_path_1 / "fruits" / "apples").touch()
-                (source_path_2 / "fruits" / "oranges").touch()
-                (source_path_1 / "vegetables" / "peppers").touch()
-                (source_path_2 / "vegetables" / "oranges").touch()
-                sut = AssetRepository(source_path_1, source_path_2)
-                assert {path async for path in sut.walk(Path("fruits"))} == {
-                    Path("fruits") / "apples",
-                    Path("fruits") / "oranges",
-                }
+        sut, source_path_1, source_path_2 = sut
+        assert {path async for path in sut.walk(Path("one"))} == {
+            Path("one") / "oranges"
+        }
+        assert {path async for path in sut.walk(Path("two"))} == {
+            Path("two") / "oranges"
+        }
+        assert {path async for path in sut.walk(Path("basket"))} == {
+            Path("basket") / "tomatoes",
+            Path("basket") / "aubergines",
+            Path("basket") / "courgettes",
+        }
