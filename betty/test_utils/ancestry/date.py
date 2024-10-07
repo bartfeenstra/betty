8039ee848dc6b4202b73ceee0a8d153810248fa5
Bart Feenstra <bart@mynameisbart.com>
2024-10-07 18:11:22 +0100

    Remove _LinkedDataObjectSchema (#2085)

--- betty/test_utils/ancestry/date.py
+++ betty/test_utils/ancestry/date.py
@@ -3,10 +3,9 @@ Test utilities for :py:mod:`betty.ancestry.date`.
 """
 
 from betty.ancestry.date import HasDate
-from betty.test_utils.ancestry import _LinkedDataObjectSchema
 
 
-class DummyHasDate(HasDate, _LinkedDataObjectSchema):
+class DummyHasDate(HasDate):
     """
     A dummy :py:class:`betty.ancestry.date.HasDate` implementation.
     """
--- betty/test_utils/ancestry/description.py
+++ betty/test_utils/ancestry/description.py
@@ -3,10 +3,9 @@ Test utilities for :py:mod:`betty.ancestry.description`.
 """
 
 from betty.ancestry.description import HasDescription
-from betty.test_utils.ancestry import _LinkedDataObjectSchema
 
 
-class DummyHasDescription(HasDescription, _LinkedDataObjectSchema):
+class DummyHasDescription(HasDescription):
     """
     A dummy :py:class:`betty.ancestry.description.HasDescription` implementation.
     """
