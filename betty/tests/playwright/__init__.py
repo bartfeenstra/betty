a863700084f35daac4c6729c92b5f46346e18ffa
Bart Feenstra <bart@bartfeenstra.com>
2025-02-19 12:57:09 +0000

    Merge Playwright tests into the main pytest directory structure (#2323)

--- betty/tests/playwright/__init__.py
+++
@@ -1,8 +0,0 @@
-from os import environ
-
-import pytest
-
-check_skip = pytest.mark.skipif(
-    environ.get("BETTY_TEST_SKIP_PLAYWRIGHT", False) is not False,
-    reason="Playwright tests are being skipped",
-)
