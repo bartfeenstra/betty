a31c02c99ee9236de17005ff7ca7e304ba8fd6db
Bart Feenstra <bart@mynameisbart.com>
2025-02-20 11:03:39 +0000

    Deprecate Cotton Candy

--- betty/tests/coverage/test_coverage.py
+++ betty/tests/coverage/test_coverage.py
@@ -329,6 +329,11 @@ _BASELINE: Mapping[str, _ModuleIgnore] = {
         "ExtensionTypeInvalidError": MissingReason.SHOULD_BE_COVERED,
         "Theme": MissingReason.STATIC_CONTENT_ONLY,
     },
+    "betty/project/extension/cotton_candy/__init__.py": {
+        "CottonCandy": {
+            "__init_subclass__": MissingReason.INHERITED,
+        },
+    },
     "betty/project/extension/cotton_candy/config.py": {
         "CottonCandyConfiguration": {
             "featured_entities": MissingReason.SHOULD_BE_COVERED,
