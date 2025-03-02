e165ae9a5bd53628b68fc83297f98f0859ca9745
Bart Feenstra <bart@mynameisbart.com>
2025-03-02 22:21:10 +0000

    Add job context IDs

--- betty/tests/test_job.py
+++ betty/tests/test_job.py
@@ -9,10 +9,15 @@ if TYPE_CHECKING:
 
 
 class TestContext:
-    async def test_cache(self, multiprocessing_manager: SyncManager) -> None:
+    def test_id(self, multiprocessing_manager: SyncManager) -> None:
+        sut = Context(manager=multiprocessing_manager)
+        assert sut.id
+        assert sut.id != Context(manager=multiprocessing_manager).id
+
+    def test_cache(self, multiprocessing_manager: SyncManager) -> None:
         sut = Context(manager=multiprocessing_manager)
         sut.cache  # noqa B018
 
-    async def test_start(self, multiprocessing_manager: SyncManager) -> None:
+    def test_start(self, multiprocessing_manager: SyncManager) -> None:
         sut = Context(manager=multiprocessing_manager)
         sut.start  # noqa B018
