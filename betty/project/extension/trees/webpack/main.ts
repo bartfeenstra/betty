d892681bb5db6a616c19d0966e9045e85c341988
Bart Feenstra <bart@mynameisbart.com>
2025-04-07 23:01:37 +0100

    Fix ESLint and Stylelint violations

--- betty/project/extension/trees/webpack/main.ts
+++ betty/project/extension/trees/webpack/main.ts
@@ -5,5 +5,5 @@ import {initializeAncestryTrees} from './trees.js'
 import {BETTY} from "@betty.py/betty/main.ts"
 
 await BETTY.addInitializer(
-    initializeAncestryTrees // eslint-disable-line @typescript-eslint/no-unsafe-argument
+    initializeAncestryTrees  
 )
