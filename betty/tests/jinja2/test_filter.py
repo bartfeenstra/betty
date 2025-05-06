48c12816517edb6bcc2517072790e0ce3fc45118
Bart Feenstra <bart@mynameisbart.com>
2025-05-06 19:31:33 +0100

    Remove the deprecated public_css and public_js Jinja2 filters

--- betty/tests/jinja2/test_filter.py
+++ betty/tests/jinja2/test_filter.py
@@ -28,7 +28,6 @@ from betty.test_utils.jinja2 import assert_template_string
 from betty.test_utils.locale.localized import DummyLocalized
 from betty.test_utils.model import DummyEntity
 from betty.tests.ancestry.test___init__ import DummyHasFileReferences
-from betty.warnings import BettyDeprecationWarning
 
 if TYPE_CHECKING:
     from collections.abc import Iterable, MutableMapping, Sequence
@@ -809,43 +808,3 @@ async def test_filter_negotiate_localizeds() -> None:
         template=template, data={"data": localizeds}, locale="nl"
     ) as (actual, _):
         assert actual == "nl"
-
-
-async def test_filter_public_css() -> None:
-    template = "{{ data | public_css }}{{ public_css_paths | safe }}"
-    with pytest.warns(BettyDeprecationWarning):
-        async with assert_template_string(
-            template=template,
-            data={"data": "betty-static:///css/my-first-css.css"},
-        ) as (actual, _):
-            assert actual == "None['/css/my-first-css.css']"
-
-
-async def test_filter_public_css__with_deprecated_url_path() -> None:
-    template = "{{ data | public_css }}{{ public_css_paths | safe }}"
-    with pytest.warns(BettyDeprecationWarning):
-        async with assert_template_string(
-            template=template,
-            data={"data": "/css/my-first-css.css"},
-        ) as (actual, _):
-            assert actual == "None['/css/my-first-css.css']"
-
-
-async def test_filter_public_js() -> None:
-    template = "{{ data | public_js }}{{ public_js_paths | safe }}"
-    with pytest.warns(BettyDeprecationWarning):
-        async with assert_template_string(
-            template=template,
-            data={"data": "betty-static:///js/my-first-js.js"},
-        ) as (actual, _):
-            assert actual == "None['/js/my-first-js.js']"
-
-
-async def test_filter_public_js__with_deprecated_url_path() -> None:
-    template = "{{ data | public_js }}{{ public_js_paths | safe }}"
-    with pytest.warns(BettyDeprecationWarning):
-        async with assert_template_string(
-            template=template,
-            data={"data": "/js/my-first-js.js"},
-        ) as (actual, _):
-            assert actual == "None['/js/my-first-js.js']"
