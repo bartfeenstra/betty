dd0e6f1bd484ab043d990bf8cdbb81fb789740e2
Bart Feenstra <bart@mynameisbart.com>
2025-05-27 17:23:58 +0100

    Fix a bug where progress would not be correctly displayed in the console

---
+++ betty/console/progress.py
@@ -0,0 +1,40 @@
+"""
+Output progress to the console.
+"""
+
+from typing import final
+
+from rich.progress import Progress as RichProgress
+from typing_extensions import override
+
+from betty.concurrent import AsynchronizedLock
+from betty.progress import Progress
+from betty.typing import internal, threadsafe
+
+
+@final
+@internal
+@threadsafe
+class ConsoleProgress(Progress):
+    """
+    Output progress to Rich.
+    """
+
+    def __init__(self, rich_progress: RichProgress, rich_task_description: str):
+        self._rich_progress = rich_progress
+        self._rich_task = self._rich_progress.add_task(
+            f"[green]{rich_task_description}"
+        )
+        self._lock = AsynchronizedLock.threading()
+        self._total = 0
+
+    @override
+    async def add(self, add: int = 1) -> None:
+        async with self._lock:
+            self._total += add
+            self._rich_progress.update(self._rich_task, total=self._total)
+
+    @override
+    async def done(self, done: int = 1) -> None:
+        async with self._lock:
+            self._rich_progress.update(self._rich_task, advance=done)
---
+++ betty/tests/console/test_progress.py
@@ -0,0 +1,19 @@
+from unittest.mock import ANY
+
+from pytest_mock import MockerFixture
+
+from betty.console.progress import ConsoleProgress
+
+
+class TestConsoleProgress:
+    async def test_add(self, mocker: MockerFixture) -> None:
+        m_rich_progress = mocker.patch("rich.progress.Progress")
+        sut = ConsoleProgress(m_rich_progress, "")
+        await sut.add(9)
+        m_rich_progress.update.assert_called_once_with(ANY, total=9)
+
+    async def test_done(self, mocker: MockerFixture) -> None:
+        m_rich_progress = mocker.patch("rich.progress.Progress")
+        sut = ConsoleProgress(m_rich_progress, "")
+        await sut.done(9)
+        m_rich_progress.update.assert_called_once_with(ANY, advance=9)
