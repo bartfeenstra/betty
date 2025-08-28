8b53f27164b9ed6e32ba40842124bf13aa9842d5
Bart Feenstra <bart@mynameisbart.com>
2025-08-28 01:21:05 +0100

    No longer test using Webkit due to random failures

--- betty/tests/project/extension/raspberry_mint/test_maps_ui.py
+++ betty/tests/project/extension/raspberry_mint/test_maps_ui.py
@@ -1,6 +1,5 @@
 from collections.abc import Iterable
 
-import pytest
 from typing_extensions import override
 
 from betty.project.extension import Extension
@@ -8,9 +7,6 @@ from betty.project.extension.raspberry_mint import RaspberryMint
 from betty.test_utils.project.extension.maps import MapsTestBase
 
 
-@pytest.mark.xfail(
-    reason="This has been failing for Webkit on Github Actions since June 25, 2025. Cause unknown."
-)
 class TestMaps(MapsTestBase):
     @override
     def get_other_extensions(self) -> Iterable[type[Extension]]:
