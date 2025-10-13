30629e98f3865b571de9a411a7d9a0cb78f41f47
Bart Feenstra <bart@mynameisbart.com>
2025-10-13 14:51:17 +0100

    Remove _bootstrap.py because it is no longer needed

--- betty/__init__.py
+++ betty/__init__.py
@@ -4,8 +4,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-from betty import _bootstrap  # noqa: F401
-
 # This lives here so it can be imported before any third-party Python modules are available.
 _ROOT_DIRECTORY_PATH = Path(__file__).resolve().parents[1]
 ROOT_DIRECTORY_PATH = _ROOT_DIRECTORY_PATH
