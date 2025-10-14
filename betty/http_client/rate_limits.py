f33ccc228c203addd0c2767275263e45e75fcf0b
Bart Feenstra <bart@mynameisbart.com>
2025-10-14 21:33:42 +0100

    Add an HTTP rate limit for the Wikipedia REST API

--- betty/http_client/rate_limits.py
+++ betty/http_client/rate_limits.py
@@ -33,3 +33,27 @@ class WikipediaActionApi(RateLimit, ClassedPlugin):
         # Foundation-managed Action APIs. We've taken the limit of "200 requests per second" from
         # https://www.mediawiki.org/wiki/Wikimedia_REST_API#Terms_and_conditions instead.
         return 200, 1
+
+
+@RateLimitDefinition(id="wikipedia-rest-api")
+class WikipediaRestApi(RateLimit, ClassedPlugin):
+    """
+    The Wikipedia REST API rate limit.
+
+    See https://www.mediawiki.org/wiki/Wikimedia_REST_API#Terms_and_conditions.
+    """
+
+    @override
+    def match(self, request: ClientRequest) -> bool:
+        return (
+            request.url.scheme in ("http", "https")
+            and request.url.host is not None
+            and request.url.host.endswith(".wikipedia.org")
+            and request.url.path == "/api/rest_v1"
+            or request.url.path.startswith("/api/rest_v1/")
+        )
+
+    @override
+    @property
+    def limit(self) -> tuple[int, int]:
+        return 200, 1
