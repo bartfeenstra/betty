0af358c1fd0d03efe48787aeb57d1f42dcc12fb2
Bart Feenstra <bart@mynameisbart.com>
2025-10-14 21:33:42 +0100

    Add an HTTP rate limit for the Wikipedia REST API

--- betty/tests/http_client/test_rate_limits.py
+++ betty/tests/http_client/test_rate_limits.py
@@ -3,7 +3,7 @@ from aiohttp.client_reqrep import ClientRequest
 from typing_extensions import override
 from yarl import URL
 
-from betty.http_client.rate_limits import WikipediaActionApi
+from betty.http_client.rate_limits import WikipediaActionApi, WikipediaRestApi
 from betty.plugin import PluginDefinition
 from betty.test_utils.http_client.rate_limit import RateLimitDefinitionTestBase
 
@@ -32,12 +32,47 @@ class TestWikipediaActionApi:
             (False, "GET", "https://example.com"),
         ],
     )
-    def test_match(self, expected: bool, method: str, url: str) -> None:
+    async def test_match(self, expected: bool, method: str, url: str) -> None:
         sut = WikipediaActionApi()
         request = ClientRequest(method, URL(url))
         assert sut.match(request) is expected
 
     def test_limit(self) -> None:
         sut = WikipediaActionApi()
+        assert sut.limit[0]
+        assert sut.limit[1]
+
+
+class TestWikipediaRestApiDefinition(RateLimitDefinitionTestBase):
+    @override
+    @pytest.fixture
+    def sut(self) -> PluginDefinition:
+        return WikipediaRestApi.plugin
+
+
+class TestWikipediaRestApi:
+    @pytest.mark.parametrize(
+        ("expected", "method", "url"),
+        [
+            (True, "GET", "https://en.wikipedia.org/api/rest_v1"),
+            (True, "GET", "https://nl.wikipedia.org/api/rest_v1"),
+            (
+                True,
+                "GET",
+                "https://en.wikipedia.org/api/rest_v1/page/summary/Amsterdam",
+            ),
+            (True, "GET", "http://en.wikipedia.org/api/rest_v1"),
+            (False, "GET", "ftp://en.wikipedia.org/api/rest_v1"),
+            (False, "GET", "https://en.wikipedia.org/api"),
+            (False, "GET", "https://example.com"),
+        ],
+    )
+    async def test_match(self, expected: bool, method: str, url: str) -> None:
+        sut = WikipediaRestApi()
+        request = ClientRequest(method, URL(url))
+        assert sut.match(request) is expected
+
+    def test_limit(self) -> None:
+        sut = WikipediaRestApi()
         assert sut.limit[0]
         assert sut.limit[1]
