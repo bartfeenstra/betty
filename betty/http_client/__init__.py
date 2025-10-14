b2f75c6d385be29491b9049debeca056088ca9c1
Bart Feenstra <bart@mynameisbart.com>
2025-10-14 16:35:32 +0100

    Log HTTP client errors

--- betty/http_client/__init__.py
+++ betty/http_client/__init__.py
@@ -1,3 +1,31 @@
 """
 The HTTP client API.
 """
+
+from aiohttp import ClientError
+from aiohttp.client_middlewares import ClientHandlerType
+from aiohttp.client_reqrep import ClientRequest, ClientResponse
+
+from betty.locale.localizable import Plain
+from betty.user import User
+
+
+class ClientErrorToUserMessageMiddleware:
+    """
+    Log client errors to a user.
+    """
+
+    def __init__(self, user: User):
+        self._user = user
+
+    async def __call__(
+        self, request: ClientRequest, handler: ClientHandlerType
+    ) -> ClientResponse:
+        """
+        Call the middleware.
+        """
+        try:
+            return await handler(request)
+        except ClientError as error:
+            await self._user.message_debug(Plain(str(error)))
+            raise
