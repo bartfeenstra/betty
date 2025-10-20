255675bc98a2b6a3d6a8a198d4e532ccb92f9aa0
Bart Feenstra <bart@mynameisbart.com>
2025-10-20 21:32:42 +0100

    Add betty.about.VERSION_MAJOR

--- betty/about.py
+++ betty/about.py
@@ -17,7 +17,20 @@ if TYPE_CHECKING:
 
 VERSION = metadata.version("betty")
 """
-The current Betty installation's version, if it has any.
+The current Betty installation's version.
+"""
+
+_VERSION_MAJOR_PARTS = VERSION.split(".")
+VERSION_MAJOR = (
+    f"0.{_VERSION_MAJOR_PARTS[1]}"
+    if _VERSION_MAJOR_PARTS[0] == "0"
+    else _VERSION_MAJOR_PARTS[0]
+)
+"""
+The current Betty installation's major version.
+
+Use this instead of :py:const:`betty.about.VERSION` in public situations, where we do not want to include minor version
+information because that may disclose security issues.
 """
 
 
@@ -36,6 +49,15 @@ Whether the current Betty installation is an unstable development version.
 VERSION_LABEL = "development" if IS_DEVELOPMENT else VERSION
 """
 The human-readable label for the current Betty installation's version.
+"""
+
+
+VERSION_MAJOR_LABEL = "development" if IS_DEVELOPMENT else VERSION_MAJOR
+"""
+The human-readable label for the current Betty installation's major version.
+
+Use this instead of :py:const:`betty.about.VERSION_LABEL` in public situations, where we do not want to include minor
+version information because that may disclose security issues.
 """
 
 
--- betty/dirs.py
+++ betty/dirs.py
@@ -7,10 +7,12 @@ that needs it, so during testing temporary, isolated directories can be used.
 
 import platformdirs
 
-from betty.about import VERSION
+from betty.about import VERSION_MAJOR
 
 _APPNAME = "betty"
 _APPAUTHOR = "betty"
 
-CACHE_DIRECTORY_PATH = platformdirs.user_cache_path(_APPNAME, _APPAUTHOR, VERSION)
-APP_CONFIG_DIRECTORY_PATH = platformdirs.user_config_path(_APPNAME, _APPAUTHOR, VERSION)
+CACHE_DIRECTORY_PATH = platformdirs.user_cache_path(_APPNAME, _APPAUTHOR, VERSION_MAJOR)
+APP_CONFIG_DIRECTORY_PATH = platformdirs.user_config_path(
+    _APPNAME, _APPAUTHOR, VERSION_MAJOR
+)
