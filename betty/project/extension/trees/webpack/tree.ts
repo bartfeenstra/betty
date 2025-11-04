5d4a43ccec0b572116d4c59731ad1b03a928dc30
Bart Feenstra <bart@mynameisbart.com>
2025-11-04 22:35:51 +0000

    Ensure Trees styling can be overridden by themes

---
+++ betty/project/extension/trees/webpack/tree.ts
@@ -0,0 +1,12 @@
+"use strict"
+
+interface TreeOptions {
+    edgeColor: string
+    nodeBackgroundColor: string
+    nodeColor: string
+    nodeHoverColor: string
+}
+
+export {
+    TreeOptions,
+}
