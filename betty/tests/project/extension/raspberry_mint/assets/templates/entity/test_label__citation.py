1dea39632c258d5cc781091f368f96be2c92ee3e
Bart Feenstra <bart@mynameisbart.com>
2025-11-12 21:12:02 +0000

    Include source names in private citation labels

--- betty/tests/project/extension/raspberry_mint/assets/templates/entity/test_label__citation.py
+++ betty/tests/project/extension/raspberry_mint/assets/templates/entity/test_label__citation.py
@@ -11,7 +11,7 @@ from betty.test_utils.jinja2 import assert_template_file
 async def test_minimal() -> None:
     source = Source()
     citation = Citation(source=source)
-    expected = f'<i>Unknown</i>, <span lang="und" dir="auto">Source {source.id}</span>'
+    expected = f'<i>Unknown</i> <sup>(<span lang="und" dir="auto">Source {source.id}</span>)</sup>'
     async with assert_template_file(
         data={
             "entity": citation,
@@ -25,7 +25,7 @@ async def test_minimal() -> None:
 async def test_with_persistent_id() -> None:
     source = Source()
     citation = Citation(id="C0", source=source)
-    expected = f'<a href="/citation/{citation.public_id}/index.html"><i>Unknown</i></a>, <span lang="und" dir="auto">Source {source.id}</span>'
+    expected = f'<a href="/citation/{citation.public_id}/index.html"><i>Unknown</i></a> <sup>(<span lang="und" dir="auto">Source {source.id}</span>)</sup>'
     async with assert_template_file(
         data={
             "entity": citation,
@@ -39,7 +39,7 @@ async def test_with_persistent_id() -> None:
 async def test_embedded() -> None:
     source = Source()
     citation = Citation(id="C0", source=source)
-    expected = f'<i>Unknown</i>, <span lang="und" dir="auto">Source {source.id}</span>'
+    expected = f'<i>Unknown</i> <sup>(<span lang="und" dir="auto">Source {source.id}</span>)</sup>'
     async with assert_template_file(
         data={
             "entity": citation,
@@ -54,7 +54,9 @@ async def test_embedded() -> None:
 async def test_with_location() -> None:
     source = Source()
     citation = Citation(source=source, location=Plain("Somewhere"))
-    expected = f'Somewhere, <span lang="und" dir="auto">Source {source.id}</span>'
+    expected = (
+        f'Somewhere <sup>(<span lang="und" dir="auto">Source {source.id}</span>)</sup>'
+    )
     async with assert_template_file(
         data={
             "entity": citation,
@@ -68,7 +70,7 @@ async def test_with_location() -> None:
 async def test_with_citation_context() -> None:
     source = Source()
     citation = Citation(id="C0", source=source)
-    expected = f'<i>Unknown</i>, <span lang="und" dir="auto">Source {source.id}</span>'
+    expected = f'<i>Unknown</i> <sup>(<span lang="und" dir="auto">Source {source.id}</span>)</sup>'
     async with assert_template_file(
         data={
             "entity": citation,
@@ -83,7 +85,7 @@ async def test_with_citation_context() -> None:
 async def test_private() -> None:
     source = Source()
     citation = Citation(source=source, private=True)
-    expected = '<span class="citation-location"><span class="private" title="This information is unavailable to protect people\'s privacy.">private</span></span>'
+    expected = f'<span class="private" title="This information is unavailable to protect people\'s privacy.">private</span> <sup>(<span lang="und" dir="auto">Source {source.id}</span>)</sup>'
     async with assert_template_file(
         data={
             "entity": citation,
