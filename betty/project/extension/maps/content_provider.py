b0f9f149496ff7feb562321cd771165943926ffc
Bart Feenstra <bart@mynameisbart.com>
2025-11-18 20:50:52 +0000

    Clean up the Map content provider

--- betty/project/extension/maps/content_provider.py
+++ betty/project/extension/maps/content_provider.py
@@ -3,16 +3,13 @@ Map content.
 """
 
 from collections.abc import Mapping
-from typing import Any, Self
+from typing import Any
 
 from typing_extensions import override
 
-from betty.config import DefaultConfigurable
 from betty.content_provider import ContentProviderDefinition
 from betty.content_provider.content_providers import Template
 from betty.locale.localizable import _
-from betty.model.config import EntityReferenceSequence
-from betty.project import Project
 from betty.resource import Context as ResourceContext
 
 
@@ -20,23 +17,10 @@ from betty.resource import Context as ResourceContext
     id="maps-map",
     label=_("Map"),
 )
-class Map(Template, DefaultConfigurable[EntityReferenceSequence]):
+class Map(Template):
     """
     An interactive map.
     """
-
-    def __init__(self, project: Project, configuration: EntityReferenceSequence):
-        super().__init__(project, configuration=configuration)
-
-    @override
-    @classmethod
-    async def new_for_project(cls, project: Project) -> Self:
-        return cls(project, configuration=cls.new_default_configuration())
-
-    @override
-    @classmethod
-    def new_default_configuration(cls) -> EntityReferenceSequence:
-        return EntityReferenceSequence()
 
     @override
     async def _provide_data(self, resource: ResourceContext) -> Mapping[str, Any]:
