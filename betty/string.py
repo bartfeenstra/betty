f00df67e25c4a8f18fae04457002ef02d89b3723
Bart Feenstra <bart@mynameisbart.com>
2025-11-23 15:41:53 +0000

    Mark positional-only arguments in the string API as such

--- betty/string.py
+++ betty/string.py
@@ -7,21 +7,21 @@ import re
 _CAMEL_CASE_PATTERN = re.compile(r"(?<!^)(?=[A-Z])")
 
 
-def camel_case_to_snake_case(string: str) -> str:
+def camel_case_to_snake_case(string: str, /) -> str:
     """
     Convert camel case to snake case.
     """
     return _CAMEL_CASE_PATTERN.sub("_", string).lower()
 
 
-def camel_case_to_kebab_case(string: str) -> str:
+def camel_case_to_kebab_case(string: str, /) -> str:
     """
     Convert camel case to kebab case.
     """
     return _CAMEL_CASE_PATTERN.sub("-", string).lower()
 
 
-def upper_camel_case_to_lower_camel_case(string: str) -> str:
+def upper_camel_case_to_lower_camel_case(string: str, /) -> str:
     """
     Convert upper camel case to lower camel case.
     """
@@ -30,14 +30,14 @@ def upper_camel_case_to_lower_camel_case(string: str) -> str:
     return string[0].lower() + string[1:]
 
 
-def snake_case_to_kebab_case(string: str) -> str:
+def snake_case_to_kebab_case(string: str, /) -> str:
     """
     Convert snake case to kebab case.
     """
     return string.replace("_", "-")
 
 
-def snake_case_to_upper_camel_case(string: str) -> str:
+def snake_case_to_upper_camel_case(string: str, /) -> str:
     """
     Convert snake case to upper camel case.
     """
@@ -47,7 +47,7 @@ def snake_case_to_upper_camel_case(string: str) -> str:
     )
 
 
-def snake_case_to_lower_camel_case(string: str) -> str:
+def snake_case_to_lower_camel_case(string: str, /) -> str:
     """
     Convert snake case to lower camel case.
     """
@@ -55,14 +55,14 @@ def snake_case_to_lower_camel_case(string: str) -> str:
     return string[0].lower() + string[1:] if string else ""
 
 
-def kebab_case_to_snake_case(string: str) -> str:
+def kebab_case_to_snake_case(string: str, /) -> str:
     """
     Convert kebab case to snake case.
     """
     return string.replace("-", "_")
 
 
-def kebab_case_to_lower_camel_case(string: str) -> str:
+def kebab_case_to_lower_camel_case(string: str, /) -> str:
     """
     Convert kebab case to lower camel case.
     """
