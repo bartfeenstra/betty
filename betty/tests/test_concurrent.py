077d8ef5eb1699c2c879c88e5417a4041d005cad
Bart Feenstra <bart@mynameisbart.com>
2026-01-25 15:50:23 +0000

    Remove AsynchronizedSemaphore

--- betty/tests/test_concurrent.py
+++ betty/tests/test_concurrent.py
@@ -2,7 +2,6 @@ import asyncio
 import threading
 import time
 from asyncio import create_task, gather, run, sleep, wait_for
-from collections.abc import Iterable
 from typing import TypeVar
 from unittest.mock import call
 
@@ -10,17 +9,7 @@ import pytest
 from pytest_mock import MockerFixture
 from typing_extensions import override
 
-from betty.concurrent import (
-    Acquirable,
-    AsynchronizedLock,
-    AsynchronizedSemaphore,
-    Ledger,
-    Lock,
-    RateLimiter,
-    Semaphore,
-    asynchronize_acquire,
-    backoff,
-)
+from betty.concurrent import AsynchronizedLock, Ledger, Lock, RateLimiter, backoff
 
 _KeyT = TypeVar("_KeyT")
 
@@ -50,72 +39,6 @@ class TestLock:
         sut = _LockTestDummyLock(False)
         with pytest.raises(asyncio.TimeoutError):
             await wait_for(sut.__aenter__(), 0.000000001)
-
-
-class _SemaphoreTestDummySemaphore(Semaphore):
-    def __init__(self, n: int, acquire: bool):
-        self._n = n
-        self._acquire = acquire
-
-    @override
-    async def acquire(self, *, wait: bool = True) -> bool:
-        if self._acquire:
-            self._n -= 1
-            return True
-        await sleep(999999999)
-        return False  # pragma: nocover
-
-    @override
-    async def release(self, n: int = 1) -> None:
-        self._n += n
-
-
-class TestSemaphore:
-    async def test___aenter____and___aexit___with_acquisition(self) -> None:
-        async with _SemaphoreTestDummySemaphore(1, True):
-            pass
-
-    async def test___aenter____and___aexit___without_acquisition(self) -> None:
-        sut = _SemaphoreTestDummySemaphore(1, False)
-        with pytest.raises(asyncio.TimeoutError):
-            await wait_for(sut.__aenter__(), 0.000000001)
-
-
-@pytest.fixture
-def acquirables() -> Iterable[Acquirable]:
-    return [
-        threading.Lock(),
-        threading.Semaphore(),
-    ]
-
-
-async def test_asynchronize_acquire__should_acquire_immediately(
-    acquirables: Iterable[Acquirable],
-) -> None:
-    for acquirable in acquirables:
-        assert await asynchronize_acquire(acquirable)
-        assert not await asynchronize_acquire(acquirable, wait=False)
-        acquirable.release()
-
-
-async def test_asynchronize_acquire__should_acquire_after_waiting(
-    acquirables: Iterable[Acquirable],
-) -> None:
-    for acquirable in acquirables:
-        acquirable.acquire()
-        task = create_task(asynchronize_acquire(acquirable))
-        await sleep(1)
-        acquirable.release()
-        assert await task
-
-
-async def test_asynchronize_acquire__should_not_acquire_if_not_waiting(
-    acquirables: Iterable[Acquirable],
-) -> None:
-    for acquirable in acquirables:
-        acquirable.acquire()
-        assert not await asynchronize_acquire(acquirable, wait=False)
-        acquirable.release()
 
 
 class TestAsynchronizedLock:
@@ -149,49 +72,6 @@ class TestAsynchronizedLock:
 
     def test_threading(self) -> None:
         AsynchronizedLock.new_threadsafe()
-
-
-class TestAsynchronizedSemaphore:
-    async def test_acquire__should_acquire_immediately(self) -> None:
-        semaphore = threading.Semaphore()
-        sut = AsynchronizedSemaphore(semaphore)
-        assert await sut.acquire(wait=False)
-        await sut.release()
-
-    async def test_acquire__should_acquire_after_waiting(self) -> None:
-        semaphore = threading.Semaphore()
-        sut = AsynchronizedSemaphore(semaphore)
-        semaphore.acquire(blocking=False)
-        task = create_task(sut.acquire())
-        await sleep(1)
-        semaphore.release()
-        assert await task
-
-    async def test_acquire__should_not_acquire_if_not_waiting(self) -> None:
-        semaphore = threading.Semaphore()
-        sut = AsynchronizedSemaphore(semaphore)
-        semaphore.acquire(blocking=False)
-        assert not await sut.acquire(wait=False)
-        semaphore.release()
-
-    async def test_release(self) -> None:
-        semaphore = threading.Semaphore(2)
-        sut = AsynchronizedSemaphore(semaphore)
-        assert await sut.acquire(wait=False)
-        assert await sut.acquire(wait=False)
-        await sut.release()
-        assert await sut.acquire(wait=False)
-        # @todo this should error
-        assert not await sut.acquire(wait=False)
-        await sut.release()
-        await sut.release()
-
-    def test_semaphore(self) -> None:
-        semaphore = threading.Semaphore()
-        assert AsynchronizedSemaphore(semaphore).semaphore is semaphore
-
-    def test_threading(self) -> None:
-        AsynchronizedSemaphore.new_threadsafe()
 
 
 class TestRateLimiter:
