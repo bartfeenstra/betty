54a0869f09b9f330acef078649f23b0ac134a3b6
Bart Feenstra <bart@mynameisbart.com>
2026-02-22 15:49:49 +0000

    Remove useless (outdated) tests

--- betty/tests/data/aggregate/record/test_mapping.py
+++ betty/tests/data/aggregate/record/test_mapping.py
@@ -6,17 +6,6 @@ from betty.test_utils.locale.localizable import DUMMY_LOCALIZABLE
 
 
 class TestTypedMappingDefinition:
-    def test_elements(self) -> None:
-        element = FieldDefinition(
-            Key("my_first_element"), StrDefinition(label=DUMMY_LOCALIZABLE)
-        )
-        sut = TypedMappingDefinition[dict[str, str]](
-            cls=dict,
-            label=DUMMY_LOCALIZABLE,
-            fields=[element],
-        )
-        assert list(sut.fields) == [element]
-
     def test_load(self) -> None:
         field_name = "my_first_element"
         sut = TypedMappingDefinition[dict[str, str]](
