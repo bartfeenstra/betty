86b7df605927954ac1340196e256b288f520aaca
Bart Feenstra <bart@mynameisbart.com>
2026-02-27 12:53:35 +0000

    Remove PluginDiscovery

--- betty/plugin/discovery/__init__.py
+++ betty/plugin/discovery/__init__.py
@@ -4,7 +4,6 @@ Plugin discovery.
 
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
 from asyncio import gather
 from collections.abc import Awaitable, Callable, Iterable
 
@@ -12,24 +11,8 @@ from betty.asyncio import resolve_await
 from betty.plugin import Plugin, PluginDefinition, ResolvableDefinition
 from betty.service.level import ServiceLevel
 
-
-class PluginDiscovery[PluginDefinitionT: PluginDefinition = PluginDefinition](ABC):
-    """
-    A plugin definition discovery.
-    """
-
-    @abstractmethod
-    async def discover(
-        self, services: ServiceLevel, /
-    ) -> Iterable[ResolvableDiscovery[PluginDefinitionT]]:
-        """
-        Discover the plugin definitions.
-        """
-
-
 type ResolvableDiscovery[PluginDefinitionT: PluginDefinition = PluginDefinition] = (
-    PluginDiscovery[PluginDefinitionT]
-    | Callable[
+    Callable[
         [ServiceLevel],
         Awaitable[Iterable["ResolvableDiscovery[PluginDefinitionT]"]]
         | Iterable["ResolvableDiscovery[PluginDefinitionT]"],
@@ -59,8 +42,6 @@ async def _discover[PluginDefinitionT: PluginDefinition](
     from betty.service.requirement import UnmetRequirement
 
     try:
-        if isinstance(discovery, PluginDiscovery):
-            return await discover(services, *await discovery.discover(services))
         if isinstance(discovery, PluginDefinition):
             return [discovery]  # ty:ignore[invalid-return-type]
         if isinstance(discovery, type) and issubclass(discovery, Plugin):
