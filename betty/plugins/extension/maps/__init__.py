41a283a9f1ace1e3f36ae969f8ada8f7377b2427
Bart Feenstra <bart@mynameisbart.com>
2026-04-21 13:52:51 +0100

    meh

--- betty/plugins/extension/maps/__init__.py
+++ betty/plugins/extension/maps/__init__.py
@@ -25,28 +25,6 @@ if TYPE_CHECKING:
     description=_("Display interactive maps"),
     requires={
         Project.assets.require(MAPS),
-        # @todo Rethink our DX, composability, and where the responsibilities lie.
-        # @todo
-        # @todo 'Nested' plugin services should not have to know about extensions, e.g. Webpack entry points
-        # @todo should just be any old service that does not need special casing to be put onto an extension.
-        # @todo
-        # @todo That also means PluginServiceRequirement is of limited use as it cannot get the correct service provider
-        # @todo from the service level.
-        # @todo
-        # @todo Either way, PluginServiceInitializer will need knowledge of how to handle plugin services on extensions
-        # @todo
-        # @todo Which means putting everything in the extension API is really not necessary.
-        # @todo Can we merge this all into PluginServiceRequirement?
-        # @todo Or at least move it closer to the core APIs?
-        # @todo
-        # @todo
-        # Project.extensions.require(Webpack.entry_points.require(MapsWebpackEntryPoint)),
-        # @todo Alternatively... (but I don't like the inside-out appearance, it reads badly)
-        # @todo However, this may be the only format that lets us get the value of the inner service
-        # @todo when calling the requirement.
-        # Webpack.entry_points.require(Project.extensions, MapsWebpackEntryPoint),
-        # @todo Alternatively...
-        # @todo Actually, this could return the inner service's value as well.
         Project.extensions.require(Webpack.entry_points, MapsWebpackEntryPoint),
     },
 )
