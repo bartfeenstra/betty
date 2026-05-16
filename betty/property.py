965a9929e37e602ff242dd2e4edf57fd585af038
Bart Feenstra <bart@mynameisbart.com>
2026-05-16 11:19:03 +0100

    Add ProxyProperty and ProxyAttr

--- betty/property.py
+++ betty/property.py
@@ -8,7 +8,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from functools import cache
 from inspect import getmembers
-from typing import TYPE_CHECKING, Any, Self, final, overload
+from typing import TYPE_CHECKING, Any, Self, final, overload, override
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -68,10 +68,11 @@ class Property[OwnerT: HasProperties, GetT](ABC):
         """
         return self.__property
 
-    def init_property_owner(self, owner: OwnerT, /) -> None:  # noqa: B027
+    def init_property_owner(self, owner: OwnerT, /) -> None:
         """
         Initialize the property on an owner.
         """
+        return
 
     @overload
     def __get__(self, instance: None, owner: type[OwnerT], /) -> Self:
@@ -92,3 +93,27 @@ class Property[OwnerT: HasProperties, GetT](ABC):
         """
         Get the property value from the owner.
         """
+
+
+class ProxyProperty[OwnerT: HasProperties, GetT](Property[OwnerT, GetT]):
+    """
+    A property that proxies another property.
+    """
+
+    def __init__(self, *args: Any, proxied: Property[OwnerT, GetT], **kwargs: Any):
+        super().__init__(*args, **kwargs)
+        self.__proxied_property = proxied
+
+    @override
+    def __set_name__(self, owner: type[OwnerT], name: str):
+        super().__set_name__(owner, name)
+        self.__proxied_property.__set_name__(owner, name)
+
+    @override
+    def get(self, owner: OwnerT, /) -> GetT:
+        return self.__proxied_property.get(owner)
+
+    @override
+    def init_property_owner(self, owner: OwnerT, /) -> None:
+        super().init_property_owner(owner)
+        self.__proxied_property.init_property_owner(owner)
