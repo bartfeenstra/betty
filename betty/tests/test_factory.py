c37b120623348a773f79df02361ddaa5d19bbc34
Bart Feenstra <bart@mynameisbart.com>
2026-08-30 13:13:04 +0100

    meh Let's try generating test fixtures...?

--- betty/tests/test_factory.py
+++ betty/tests/test_factory.py
@@ -1,10 +1,11 @@
-from collections.abc import Callable, Iterator, Sequence
+from collections.abc import Callable, Iterable, Iterator, Sequence
 from textwrap import indent
-from typing import Any, Final, Self, override
+from typing import Any, Final, final
 
 import pytest
 
-from betty.factory import Arg1Manufacturable, FactoryError, Manufacturable, new
+from betty.factory import FactoryError, _arg_counts_to_manufacturables, new
+from betty.string import snake_case_to_upper_camel_case
 
 
 class _Arg1:
@@ -25,50 +26,24 @@ _args = (_arg1, _arg2)
 
 
 class _Value:
+    @final
     def __init__(self, *args: Any):
         self.args: Final[tuple[Any]] = args
 
+    @final
+    def __eq__(self, other: Any, /) -> bool:
+        if not isinstance(other, type(self)):
+            return NotImplemented
+        return self.args == other.args
+
+    @final
     def __repr__(self) -> str:
         return f"<{type(self).__name__!r} args={self.args!r}>"
 
 
-class _Arg1Manufacturer(_Value):
-    pass
-
-
-class _Arg1WithVariadicArgsAndKwargsManufacturer(_Value):
-    def __init__(self, *args: Any, **kwargs: Any):
-        raise NotImplementedError
-
-
-class _Arg1WithArg1WithVariadicArgsAndKwargsManufacturer(_Value):
-    def __init__(self, arg1: _Arg1, /, *args: Any, **kwargs: Any):
-        raise NotImplementedError
-
-
-class _Arg1WithArg1WithDefaultArgAndKwargManufacturer(_Value):
-    def __init__(self, arg1: _Arg1, arg: None = None, /, *, kwarg: None = None):
-        raise NotImplementedError
-
-
-class _Arg1Manufacturable(Manufacturable):
-    @override
-    @classmethod
-    async def new(cls) -> Self:
-        return cls()
-
-
-class _Arg1Arg1Manufacturable(Arg1Manufacturable[_Arg1]):
-    @override
-    @classmethod
-    async def new(cls, arg1: _Arg1, /) -> Self:
-        assert arg1 is _arg1
-        return cls()
-
-
 def _parameterize_test_new(
     max_new_arg_count: int, /
-) -> Iterator[tuple[type[_Value], Any, tuple[Any, ...]]]:
+) -> Iterator[tuple[_Value, Any, tuple[Any, ...]]]:
     assert max_new_arg_count > 1
     for new_arg_count in range(max_new_arg_count):
         for test_parameters in _parameterize_test_new_arg_count(new_arg_count):
@@ -77,7 +52,7 @@ def _parameterize_test_new(
 
 def _parameterize_test_new_arg_count(
     new_arg_count: int, /
-) -> Iterator[tuple[type[_Value], Any]]:
+) -> Iterator[tuple[_Value, Any]]:
     for manufacturer_arg_count in range(new_arg_count + 1):
         manufacturer_parameters = tuple(
             f"arg{manufacturer_arg_number}: _Arg{manufacturer_arg_number}"
@@ -88,8 +63,11 @@ def _parameterize_test_new_arg_count(
             for manufacturer_arg_number in range(1, manufacturer_arg_count)
         )
         # @todo For each of the following variations here in the loop, also create classes with a .new() with the same signature.
-        yield from _create_new_manufacturer_functions(
-            "with_individual_args", manufacturer_parameters, manufacturer_arg_mappers
+        yield from _create_new_manufacturer_functions_and_manufacturable_class(
+            new_arg_count,
+            "with_individual_args",
+            manufacturer_parameters,
+            manufacturer_arg_mappers,
         )
         yield from _create_new_manufacturer_functions(
             "with_individual_positional_only_args",
@@ -150,11 +128,11 @@ def _parameterize_test_new_arg_count(
     )
 
 
-def _create(name: str, code: str) -> Any:
+def _create_from_source(name: str, source: str, /) -> Any:
     locals_ = {}
-    exec(code, locals=locals_)
+    exec(source, locals=locals_)
     created = locals_[name]
-    created._betty_test_source = code
+    created._betty_test_source = source
     return created
 
 
@@ -162,46 +140,100 @@ def _create_new_function_args(args: Sequence[str], /) -> str:
     return "\n".join(map(lambda line: f"{line},", args))
 
 
-def _create_new_function(
+def _create_new_function_source(
     name: str,
     parameters: Sequence[str],
     return_type: str | None,
     body: str,
     sync: bool = True,
     /,
-) -> Callable:
-    code = ""
+) -> tuple[str, str]:
+    source = ""
     if sync:
         name += "_sync"
     else:
         name += "_async"
-        code += "async "
-    code += "def "
-    code += name
-    code += "("
+        source += "async "
+    source += "def "
+    source += name
+    source += "("
     if parameters:
-        code += "\n"
-        code += indent(_create_new_function_args(parameters), "  ")
-        code += "\n"
-    code += ")"
+        source += "\n"
+        source += indent(_create_new_function_args(parameters), "  ")
+        source += "\n"
+    source += ")"
     if return_type is not None:
-        code += " -> " + return_type
-    code += ":\n"
-    code += indent(body, "  ")
-    return _create(name, code)
+        source += " -> " + return_type
+    source += ":\n"
+    source += indent(body, "  ")
+    return name, source
 
 
 def _create_new_manufacturer_functions(
     name: str, parameters: Sequence[str], arg_mappers: Sequence[str], /
-) -> Any:
+) -> Iterable[tuple[_Value, Callable]]:
     name = f"function_with_{len(arg_mappers)}_manufacturer_args_{name}"
-    body = f"""return _Value(
+    body_source = f"""return _Value(
 {indent(_create_new_function_args(arg_mappers), "  ")}
 )
 """
-    expected = _args[: len(arg_mappers)]
-    yield (expected, _create_new_function(name, parameters, "_Value", body, True))
-    yield (expected, _create_new_function(name, parameters, "_Value", body, False))
+    expected = _Value(*_args[: len(arg_mappers)])
+    yield (
+        expected,
+        _create_from_source(
+            *_create_new_function_source(name, parameters, "_Value", body_source, True),
+        ),
+    )
+    yield (
+        expected,
+        _create_from_source(
+            *_create_new_function_source(
+                name, parameters, "_Value", body_source, False
+            ),
+        ),
+    )
+
+
+def _create_new_manufacturable_class(
+    new_arg_count: int, name: str, parameters: Sequence[str], arg_mappers: Sequence[str]
+) -> tuple[_Value, type]:
+    # @todo
+    name = f"ClassWith{len(arg_mappers)}ManufacturerArgs{snake_case_to_upper_camel_case(name)}"
+    new_body_source = f"""return cls(
+{indent(_create_new_function_args(arg_mappers), "  ")}
+)
+"""
+    source = f"""class {name}({
+        _arg_counts_to_manufacturables[new_arg_count][0][0]
+    }, _Value):
+{
+        indent(
+            _create_new_function_source(
+                # @todo _create_new_function_source() alters the name with a _sync/_async suffix...
+                # @todo
+                # @todo
+                "new",
+                parameters,
+                None,
+                new_body_source,
+                False,
+            ),
+            "  ",
+        )
+    }
+"""
+    return _Value(*_args[: len(arg_mappers)]), _create_from_source(name, source)
+
+
+def _create_new_manufacturer_functions_and_manufacturable_class(
+    new_arg_count: int,
+    name: str,
+    parameters: Sequence[str],
+    arg_mappers: Sequence[str],
+    /,
+) -> Iterable[tuple[_Value, Callable | type]]:
+    yield from _create_new_manufacturer_functions(name, parameters, arg_mappers)
+    yield _create_new_manufacturable_class(new_arg_count, name, parameters, arg_mappers)
 
 
 @pytest.mark.parametrize(
@@ -209,7 +241,7 @@ def _create_new_manufacturer_functions(
     list(_parameterize_test_new(2)),
 )
 async def test_new(
-    expected: tuple[Any, ...], manufacturer: Any, new_args: tuple[Any, ...]
+    expected: _Value, manufacturer: Any, new_args: tuple[Any, ...]
 ) -> None:
     manufacturer_message = (
         f"\nnew() *args: {new_args!r}\nSource code:\n{manufacturer._betty_test_source}"
@@ -221,6 +253,6 @@ async def test_new(
             f"{manufacturer!r} raised an unexpected error: {error.__cause__}{manufacturer_message}"
         ) from error
     else:
-        assert value.args == expected, (
-            f"{manufacturer!r} returned {value} but {_Value(*expected)!r} was expected.{manufacturer_message}"
+        assert value == expected, (
+            f"{manufacturer!r} returned {value} but {expected!r} was expected.{manufacturer_message}"
         )
