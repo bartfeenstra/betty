db36e04deb5407ba3951187b9be4df9dca33b5df
Bart Feenstra <bart@mynameisbart.com>
2026-09-12 14:35:00 +0100

    Remove the constraint preventing aiohttp 3.14 or newer from being installed

--- betty/tests/conftest.py
+++ betty/tests/conftest.py
@@ -4,9 +4,13 @@ Pytest configuration.
 
 from __future__ import annotations
 
+from inspect import signature
 from os import environ
+from typing import Any
+from unittest.mock import Mock
 from warnings import filterwarnings
 
+import aiohttp
 import pytest
 
 from betty.test_utils.conftest import *  # noqa: F403
@@ -33,3 +37,15 @@ check_skip_webpack_entry_point_provider = pytest.mark.skipif(
     bool(environ.get("BETTY_TEST_SKIP_WEBPACK_ENTRY_POINT_PROVIDER", "")),
     reason="Webpack entry point provider tests are being skipped",
 )
+
+
+# Work around a problem where aiohttp 3.14 added a required kwarg ``stream_writer``, which aioresponses does not yet
+# support.
+_aiohttp_client_response_init = aiohttp.ClientResponse.__init__
+if "stream_writer" in signature(_aiohttp_client_response_init).parameters:
+
+    def _init(*args: Any, **kwargs: Any) -> None:
+        kwargs.setdefault("stream_writer", Mock(output_size=0))
+        _aiohttp_client_response_init(*args, **kwargs)
+
+    aiohttp.ClientResponse.__init__ = _init
